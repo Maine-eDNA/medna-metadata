5015db1a4e87f79e1af40058bdbfa469ac9d2838
Mel <mkimble88@gmail.com>
2021-08-06 15:01:43 -0400

    updated field_sites resources.py to reflect changes to models.py

--- field_survey/resources.py
+++ field_survey/resources.py
@@ -1,6 +1,6 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
-from .models import FieldSurvey, FieldCrew, EnvMeasurement, FieldCollection, SampleFilter
+from .models import FieldSurvey, FieldCrew, EnvMeasurement, FieldCollection, FieldSample
 from django.contrib.auth.models import User
 
 class FieldSurveyAdminResource(resources.ModelResource):
@@ -104,28 +104,28 @@ class EnvMeasurementAdminResource(resources.ModelResource):
     class Meta:
         # SampleLabel
         model = EnvMeasurement
-        import_id_fields = ('env_global_id', 'env_measure_time', 'env_measure_depth', 'env_instrument','env_ctd_filename',
-                            'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn', 'env_ysi_notes',
-                            'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number', 'env_niskin_notes',
-                            'env_inst_other', 'env_meas_taken', 'env_flow_rate', 'env_water_temp', 'env_salinity',
-                            'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity', 'env_do',
-                            'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
-                            'env_substrate', 'env_lab_datetime', 'env_conditions_notes', 'survey_global_id', )
-        fields = ('env_global_id', 'env_measure_time', 'env_measure_depth', 'env_instrument','env_ctd_filename',
-                  'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn', 'env_ysi_notes',
-                  'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number', 'env_niskin_notes',
-                  'env_inst_other', 'env_meas_taken', 'env_flow_rate', 'env_water_temp', 'env_salinity',
-                  'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity', 'env_do',
-                  'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
-                  'env_substrate', 'env_lab_datetime', 'env_conditions_notes', 'survey_global_id',
+        import_id_fields = ('env_global_id', 'env_measure_datetime', 'env_measure_depth', 'env_instrument',
+                            'env_ctd_filename', 'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn',
+                            'env_ysi_notes', 'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number',
+                            'env_niskin_notes', 'env_inst_other', 'env_measurement', 'env_flow_rate', 'env_water_temp',
+                            'env_salinity', 'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity',
+                            'env_do', 'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
+                            'env_substrate', 'env_lab_datetime', 'env_measure_notes', 'survey_global_id', )
+        fields = ('env_global_id', 'env_measure_datetime', 'env_measure_depth', 'env_instrument',
+                  'env_ctd_filename', 'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn',
+                  'env_ysi_notes', 'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number',
+                  'env_niskin_notes', 'env_inst_other', 'env_measurement', 'env_flow_rate', 'env_water_temp',
+                  'env_salinity', 'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity',
+                  'env_do', 'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
+                  'env_substrate', 'env_lab_datetime', 'env_measure_notes', 'survey_global_id',
                   'created_by', 'created_datetime',)
-        export_order = ('env_global_id', 'env_measure_time', 'env_measure_depth', 'env_instrument','env_ctd_filename',
-                        'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn', 'env_ysi_notes',
-                        'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number', 'env_niskin_notes',
-                        'env_inst_other', 'env_meas_taken', 'env_flow_rate', 'env_water_temp', 'env_salinity',
-                        'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity', 'env_do',
-                        'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
-                        'env_substrate', 'env_lab_datetime', 'env_conditions_notes', 'survey_global_id',
+        export_order = ('env_global_id', 'env_measure_datetime', 'env_measure_depth', 'env_instrument',
+                        'env_ctd_filename', 'env_ctd_notes', 'env_ysi_filename', 'env_ysi_model', 'env_ysi_sn',
+                        'env_ysi_notes', 'env_secchi_depth', 'env_secchi_notes', 'env_niskin_number',
+                        'env_niskin_notes', 'env_inst_other', 'env_measurement', 'env_flow_rate', 'env_water_temp',
+                        'env_salinity', 'env_ph_scale', 'env_par1', 'env_par2', 'env_turbidity', 'env_conductivity',
+                        'env_do', 'env_pheophytin', 'env_chla', 'env_no3no2', 'env_no2', 'env_nh4', 'env_phosphate',
+                        'env_substrate', 'env_lab_datetime', 'env_measure_notes', 'survey_global_id',
                         'created_by', 'created_datetime',)
 
     survey_global_id = fields.Field(
@@ -145,33 +145,27 @@ class FieldCollectionAdminResource(resources.ModelResource):
     class Meta:
         # SampleLabel
         model = FieldCollection
-        import_id_fields = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type','water_vessel_label',
-                            'water_collect_time', 'water_collect_depth', 'water_collect_mode', 'water_niskin_number',
-                            'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material', 'water_vessel_color',
-                            'water_collect_notes','core_control', 'core_label', 'core_start', 'core_end', 'core_method',
-                            'core_method_other','core_collect_depth', 'core_length', 'core_diameter', 'subcores_taken',
-                            'subcore_fname','subcore_lname', 'subcore_method', 'subcore_method_other', 'subcore_date',
-                            'subcore_start','subcore_end', 'subcore_min_barcode', 'subcore_num', 'subcore_length',
-                            'subcore_diameter','subcore_clayer', 'core_purpose', 'core_notes', 'was_prefiltered',
-                            'was_filtered','survey_global_id', )
-        fields = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type','water_vessel_label',
-                  'water_collect_time', 'water_collect_depth', 'water_collect_mode', 'water_niskin_number',
-                  'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material', 'water_vessel_color',
-                  'water_collect_notes','core_control', 'core_label', 'core_start', 'core_end', 'core_method',
-                  'core_method_other','core_collect_depth', 'core_length', 'core_diameter', 'subcores_taken',
-                  'subcore_fname','subcore_lname', 'subcore_method', 'subcore_method_other', 'subcore_date',
-                  'subcore_start','subcore_end', 'subcore_min_barcode', 'subcore_num', 'subcore_length',
-                  'subcore_diameter','subcore_clayer', 'core_purpose', 'core_notes', 'was_prefiltered',
-                  'was_filtered','survey_global_id', 'created_by', 'created_datetime',)
-        export_order = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type','water_vessel_label',
-                        'water_collect_time', 'water_collect_depth', 'water_collect_mode', 'water_niskin_number',
-                        'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material', 'water_vessel_color',
-                        'water_collect_notes','core_control', 'core_label', 'core_start', 'core_end', 'core_method',
-                        'core_method_other','core_collect_depth', 'core_length', 'core_diameter', 'subcores_taken',
-                        'subcore_fname','subcore_lname', 'subcore_method', 'subcore_method_other', 'subcore_date',
-                        'subcore_start','subcore_end', 'subcore_min_barcode', 'subcore_num', 'subcore_length',
-                        'subcore_diameter','subcore_clayer', 'core_purpose', 'core_notes', 'was_prefiltered',
-                        'was_filtered','survey_global_id', 'created_by', 'created_datetime',)
+        import_id_fields = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type',
+                            'water_vessel_label', 'water_collect_datetime', 'water_collect_depth', 'water_collect_mode',
+                            'water_niskin_number', 'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material',
+                            'water_vessel_color', 'water_collect_notes', 'was_filtered', 'core_control', 'core_label',
+                            'core_datetime_start', 'core_datetime_end', 'core_method', 'core_method_other',
+                            'core_collect_depth', 'core_length', 'core_diameter', 'core_purpose', 'core_notes',
+                            'subcores_taken', 'survey_global_id', )
+        fields = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type',
+                  'water_vessel_label', 'water_collect_datetime', 'water_collect_depth', 'water_collect_mode',
+                  'water_niskin_number', 'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material',
+                  'water_vessel_color', 'water_collect_notes', 'was_filtered', 'core_control', 'core_label',
+                  'core_datetime_start', 'core_datetime_end', 'core_method', 'core_method_other',
+                  'core_collect_depth', 'core_length', 'core_diameter', 'core_purpose', 'core_notes',
+                  'subcores_taken', 'survey_global_id', 'created_by', 'created_datetime',)
+        export_order = ('collection_global_id', 'collection_type', 'water_control', 'water_control_type',
+                        'water_vessel_label', 'water_collect_datetime', 'water_collect_depth', 'water_collect_mode',
+                        'water_niskin_number', 'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material',
+                        'water_vessel_color', 'water_collect_notes', 'was_filtered', 'core_control', 'core_label',
+                        'core_datetime_start', 'core_datetime_end', 'core_method', 'core_method_other',
+                        'core_collect_depth', 'core_length', 'core_diameter', 'core_purpose', 'core_notes',
+                        'subcores_taken', 'survey_global_id', 'created_by', 'created_datetime',)
 
     survey_global_id = fields.Field(
         column_name='survey_global_id',
@@ -185,22 +179,31 @@ class FieldCollectionAdminResource(resources.ModelResource):
     def before_import_row(self, row, **kwargs):
         row['created_by'] = kwargs['user'].id
 
-class SampleFilterAdminResource(resources.ModelResource):
+class FieldSampleAdminResource(resources.ModelResource):
     class Meta:
         # SampleLabel
-        model = SampleFilter
-        import_id_fields = ('filter_global_id','filter_location','filter_fname', 'filter_lname', 'filter_sample_label',
-                            'filter_barcode', 'filter_date', 'filter_time', 'filter_method', 'filter_method_other',
+        model = FieldSample
+        import_id_fields = ('sample_global_id', 'field_sample_barcode', 'is_extracted',
+                            'sample_type', 'filter_location', 'is_prefilter', 'filter_fname', 'filter_lname',
+                            'filter_sample_label', 'filter_datetime', 'filter_method', 'filter_method_other',
                             'filter_vol', 'filter_type', 'filter_type_other', 'filter_pore', 'filter_size',
-                            'filter_notes', 'collection_global_id',)
-        fields = ('filter_global_id','filter_location','filter_fname', 'filter_lname', 'filter_sample_label',
-                  'filter_barcode', 'filter_date', 'filter_time', 'filter_method', 'filter_method_other',
+                            'filter_notes', 'subcore_fname', 'subcore_lname', 'subcore_method', 'subcore_method_other',
+                            'subcore_datetime_start', 'subcore_datetime_end', 'subcore_number', 'subcore_length',
+                            'subcore_diameter', 'subcore_clayer', 'collection_global_id', )
+        fields = ('sample_global_id', 'field_sample_barcode', 'is_extracted',
+                  'sample_type', 'filter_location', 'is_prefilter', 'filter_fname', 'filter_lname',
+                  'filter_sample_label', 'filter_datetime', 'filter_method', 'filter_method_other',
                   'filter_vol', 'filter_type', 'filter_type_other', 'filter_pore', 'filter_size',
-                  'filter_notes', 'collection_global_id', 'created_datetime', 'created_by',)
-        export_order = ('filter_global_id','filter_location','filter_fname', 'filter_lname', 'filter_sample_label',
-                        'filter_barcode', 'filter_date', 'filter_time', 'filter_method', 'filter_method_other',
+                  'filter_notes', 'subcore_fname', 'subcore_lname', 'subcore_method', 'subcore_method_other',
+                  'subcore_datetime_start', 'subcore_datetime_end', 'subcore_number', 'subcore_length',
+                  'subcore_diameter', 'subcore_clayer', 'collection_global_id', 'created_datetime', 'created_by',)
+        export_order = ('sample_global_id', 'field_sample_barcode', 'is_extracted',
+                        'sample_type', 'filter_location', 'is_prefilter', 'filter_fname', 'filter_lname',
+                        'filter_sample_label', 'filter_datetime', 'filter_method', 'filter_method_other',
                         'filter_vol', 'filter_type', 'filter_type_other', 'filter_pore', 'filter_size',
-                        'filter_notes', 'collection_global_id', 'created_datetime', 'created_by', )
+                        'filter_notes', 'subcore_fname', 'subcore_lname', 'subcore_method', 'subcore_method_other',
+                        'subcore_datetime_start', 'subcore_datetime_end', 'subcore_number', 'subcore_length',
+                        'subcore_diameter', 'subcore_clayer', 'collection_global_id', 'created_datetime', 'created_by',)
 
     collection_global_id = fields.Field(
         column_name='collection_global_id',
