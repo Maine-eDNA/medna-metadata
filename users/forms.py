48afcfd13fa6796a71d902dcc58cc566913d6fe7
Mel <mkimble88@gmail.com>
2021-08-06 15:02:03 -0400

    removed whitespace

--- users/forms.py
+++ users/forms.py
@@ -2,14 +2,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
-
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('email',)
 
-
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email','first_name', 'last_name', 'phone_number')
+        fields = ('email', 'first_name', 'last_name', 'phone_number')
--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-
 class UsersManagersTests(TestCase):
-
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(email='normal@user.com', password='foo')
