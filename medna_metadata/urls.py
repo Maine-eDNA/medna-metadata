27ee5ad04a8b8392d32ef6be5c5f50b2d42f36b6
Mel <mkimble88@gmail.com>
2021-08-09 10:08:03 -0400

    added field_survey and wet_lab viewsets

--- medna_metadata/urls.py
+++ medna_metadata/urls.py
@@ -21,14 +21,43 @@ from rest_framework import routers
 from users.views import CustomUserViewSet
 from field_sites.views import FieldSitesViewSet
 from sample_labels.views import SampleLabelRequestViewSet, SampleLabelViewSet
+from field_survey.views import FieldSurveyViewSet, FieldCrewViewSet, EnvMeasurementViewSet, FieldCollectionViewSet, \
+    FieldSampleViewSet
+from wet_lab.views import PrimerPairViewSet, IndexPairViewSet, IndexRemovalMethodViewSet, SizeSelectionMethodViewSet, \
+    QuantificationMethodViewSet, ExtractionMethodViewSet, ExtractionViewSet, DdpcrViewSet, QpcrViewSet, \
+    LibraryPrepViewSet, PooledLibraryViewSet, FinalPooledLibraryViewSet, RunPrepViewSet, RunResultViewSet, \
+    FastqFileViewSet
 
 router = routers.DefaultRouter()
+# users
 router.register(r'users', CustomUserViewSet, 'users')
+# field sites
 router.register(r'field_sites', FieldSitesViewSet, 'field_sites')
+# sample_labels
 router.register(r'sample_label_requests', SampleLabelRequestViewSet, 'sample_labels')
 router.register(r'sample_labels', SampleLabelViewSet, 'sample_labels')
-
-
+# field_survey
+router.register(r'field_survey', FieldSurveyViewSet, 'field_survey')
+router.register(r'field_survey', FieldCrewViewSet, 'field_survey')
+router.register(r'field_survey', EnvMeasurementViewSet, 'field_survey')
+router.register(r'field_survey', FieldCollectionViewSet, 'field_survey')
+router.register(r'field_survey', FieldSampleViewSet, 'field_survey')
+# wet_lab
+router.register(r'wet_lab', PrimerPairViewSet, 'wet_lab')
+router.register(r'wet_lab', IndexPairViewSet, 'wet_lab')
+router.register(r'wet_lab', IndexRemovalMethodViewSet, 'wet_lab')
+router.register(r'wet_lab', SizeSelectionMethodViewSet, 'wet_lab')
+router.register(r'wet_lab', QuantificationMethodViewSet, 'wet_lab')
+router.register(r'wet_lab', ExtractionMethodViewSet, 'wet_lab')
+router.register(r'wet_lab', ExtractionViewSet, 'wet_lab')
+router.register(r'wet_lab', DdpcrViewSet, 'wet_lab')
+router.register(r'wet_lab', QpcrViewSet, 'wet_lab')
+router.register(r'wet_lab', LibraryPrepViewSet, 'wet_lab')
+router.register(r'wet_lab', PooledLibraryViewSet, 'wet_lab')
+router.register(r'wet_lab', FinalPooledLibraryViewSet, 'wet_lab')
+router.register(r'wet_lab', RunPrepViewSet, 'wet_lab')
+router.register(r'wet_lab', RunResultViewSet, 'wet_lab')
+router.register(r'wet_lab', FastqFileViewSet, 'wet_lab')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
