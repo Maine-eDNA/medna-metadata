0c1888adb82752c4e5ab90c5e7ce1d6a0132ab96
Mel <mkimble88@gmail.com>
2021-08-09 18:16:49 -0400

    fixed style formatting and changed links to urlfields

--- bioinfo_denoising/models.py
+++ bioinfo_denoising/models.py
@@ -2,8 +2,10 @@ from django.contrib.gis.db import models
 from wet_lab.models import RunResult
 from users.models import DateTimeUserMixin
 
+
 # Create your models here.
 class DenoisingMethod(DateTimeUserMixin):
+    # DADA2, DEBLUR, PYRONOISE, UNOISE3
     denoising_method_name = models.CharField("Denoising Method Name", max_length=255)
     denoising_method_pipeline = models.CharField("Denoising Pipeline", max_length=255)
 
@@ -12,33 +14,37 @@ class DenoisingMethod(DateTimeUserMixin):
             pipeline=self.denoising_method_pipeline,
             name=self.denoising_method_name)
 
+
 class DenoisingMetadata(DateTimeUserMixin):
     run_result = models.ForeignKey(RunResult, on_delete=models.RESTRICT)
     analysis_date = models.DateField("Analysis Date", blank=True, null=True)
     analyst_first_name = models.CharField("Analyst First Name", max_length=255)
     analyst_last_name = models.CharField("Analyst Last Name", max_length=255)
-    analysis_sop_filename = models.TextField("Analysis SOP Filename")
-    analysis_script_repo_url = models.URLField("Repository URL", max_length=200, default="https://github.com/Maine-eDNA")
     analysis_method = models.ForeignKey(DenoisingMethod, on_delete=models.RESTRICT)
-    readme_datafile = models.TextField("README Datafile")
+    analysis_sop_url = models.URLField("Analysis SOP URL")
+    analysis_script_repo_url = models.URLField("Repository URL", max_length=200,
+                                               default="https://github.com/Maine-eDNA")
 
     def __str__(self):
         return '{date}, {fname} {lname}, {method}'.format(
             date=self.analysis_date,
             fname=self.analyst_first_name,
             lname=self.analyst_last_name,
-            method=self.analysis_method)
+            method=self.analysis_method.denoising_method_name)
+
 
 class AmpliconSequenceVariant(DateTimeUserMixin):
     denoising_metadata = models.ForeignKey(DenoisingMetadata, on_delete=models.RESTRICT)
-    amplicon_sequence_variant = models.TextField("Amplicon Sequence Variant (ASV)")
+    asv_id = models.TextField("ASV ID")
+    asv_sequence = models.TextField("ASV Sequence")
 
     def __str__(self):
         return '{id}: {date}, {method}, {asv}'.format(
             id=self.denoising_metadata.pk,
             date=self.denoising_metadata.analysis_date,
             method=self.denoising_metadata.analysis_method,
-            asv=self.amplicon_sequence_variant)
+            asv=self.asv_sequence)
+
 
 class ASVRead(DateTimeUserMixin):
     asv = models.ForeignKey(AmpliconSequenceVariant, on_delete=models.RESTRICT)
@@ -48,4 +54,4 @@ class ASVRead(DateTimeUserMixin):
         return '{id}: {num_reads}, {asv} '.format(
             id=self.asv.denoising_metadata.pk,
             num_reads=self.number_reads,
-            asv=self.asv.amplicon_sequence_variant)
+            asv=self.asv.asv_sequence)
