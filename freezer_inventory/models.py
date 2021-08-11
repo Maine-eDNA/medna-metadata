e90889d11e9c30c782bcc3e2cc5685dec726fb21
Mel <mkimble88@gmail.com>
2021-08-11 14:15:12 -0400

    added row, col, depth to __str__ and moved freezer_inv_status_update method outside of class

--- freezer_inventory/models.py
+++ freezer_inventory/models.py
@@ -1,6 +1,5 @@
 from django.contrib.gis.db import models
 from django.utils.text import slugify
-
 import datetime
 from django.contrib.auth import get_user_model
 from users.models import get_default_user
@@ -10,9 +9,21 @@ from users.models import DateTimeUserMixin, get_sentinel_user
 from users.enumerations import MeasureUnits, VolUnits, InvStatus, InvTypes, CheckoutActions
 
 
+def freezer_inv_status_update(inv_pk, freezer_checkout_action):
+    if freezer_checkout_action == CheckoutActions.CHECKOUT:
+        # Checkout, so change inventory status to Checked Out
+        FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=InvStatus.OUT)
+    elif freezer_checkout_action == CheckoutActions.RETURN:
+        # Return, so change inventory status to In Stock
+        FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=InvStatus.IN)
+    elif freezer_checkout_action == CheckoutActions.REMOVE:
+        # Removed, so change inventory status to Permanently Removed
+        FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=InvStatus.REMOVED)
+
+
 # Create your models here.
 class Freezer(DateTimeUserMixin):
-    freezer_date = models.DateTimeField("Freezer DateTime", auto_now=True)
+    freezer_datetime = models.DateTimeField("Freezer DateTime", auto_now=True)
     freezer_label = models.CharField("Freezer Label", max_length=255)
     freezer_depth = models.DecimalField("Freezer Depth", max_digits=10, decimal_places=10)
     freezer_length = models.DecimalField("Freezer Length", max_digits=10,  decimal_places=10)
@@ -24,12 +35,15 @@ class Freezer(DateTimeUserMixin):
     freezer_max_depth = models.PositiveIntegerField("Max Freezer Depth (Boxes)")
 
     def __str__(self):
-        return self.freezer_label
+        return '{label} [r{row}, c{column}, d{depth}]'.format(label=self.freezer_label,
+                                                              row=self.freezer_max_rows,
+                                                              column=self.freezer_max_columns,
+                                                              depth=self.freezer_max_depth)
 
 
 class FreezerRack(DateTimeUserMixin):
     freezer = models.ForeignKey(Freezer, on_delete=models.RESTRICT)
-    freezer_rack_date = models.DateTimeField("Freezer Rack DateTime", auto_now=True)
+    freezer_rack_datetime = models.DateTimeField("Freezer Rack DateTime", auto_now=True)
     freezer_rack_label = models.CharField("Freezer Rack Label", max_length=255)
     # location of rack in freezer
     freezer_rack_column_start = models.PositiveIntegerField("Freezer Rack Column Start")
@@ -40,7 +54,15 @@ class FreezerRack(DateTimeUserMixin):
     freezer_rack_depth_end = models.PositiveIntegerField("Freezer Rack Depth End")
 
     def __str__(self):
-        return self.freezer_rack_label
+        return '{label} [r{row_start}:{row_end}, ' \
+               'c{column_start}:{column_end}, ' \
+               'd{depth_start}:{depth_end}]'.format(label=self.freezer_rack_label,
+                                                    row_start=self.freezer_rack_column_start,
+                                                    row_end=self.freezer_rack_column_end,
+                                                    column_start=self.freezer_rack_column_start,
+                                                    column_end=self.freezer_rack_column_end,
+                                                    depth_start=self.freezer_rack_depth_start,
+                                                    depth_end=self.freezer_rack_depth_end)
 
 
 class FreezerBox(DateTimeUserMixin):
@@ -53,7 +75,10 @@ class FreezerBox(DateTimeUserMixin):
     freezer_box_depth = models.PositiveIntegerField("Freezer Box Depth")
 
     def __str__(self):
-        return self.freezer_box_label
+        return '{label} [r{row}, c{column}, d{depth}]'.format(label=self.freezer_box_label,
+                                                              row=self.freezer_box_row,
+                                                              column=self.freezer_box_column,
+                                                              depth=self.freezer_box_depth)
 
 
 class FreezerInventory(DateTimeUserMixin):
@@ -62,41 +87,39 @@ class FreezerInventory(DateTimeUserMixin):
     extraction = models.OneToOneField(Extraction, on_delete=models.RESTRICT, blank=True)
     barcode_slug = models.SlugField(max_length=27, null=True)
     freezer_inventory_datetime = models.DateTimeField("Freezer Inventory Date", auto_now=True)
-    freezer_inventory_type = models.IntegerField("Freezer Inventory Type", choices=InvTypes.choices)
-    freezer_inventory_status = models.IntegerField("Freezer Inventory Status", choices=InvStatus.choices, default=InvStatus.IN)
+    freezer_inventory_type = models.IntegerField("Freezer Inventory Type",
+                                                 choices=InvTypes.choices)
+    freezer_inventory_status = models.IntegerField("Freezer Inventory Status",
+                                                   choices=InvStatus.choices,
+                                                   default=InvStatus.IN)
     # location of inventory in freezer box
     freezer_inventory_column = models.PositiveIntegerField("Freezer Box Column")
     freezer_inventory_row = models.PositiveIntegerField("Freezer Box Row")
 
     def save(self, *args, **kwargs):
-        # if it already exists we don't want to change the site_id; we only want to update the associated fields.
-        if self.pk is None:
-            if self.freezer_inventory_type == InvTypes.EXTRACTION:
-
-                # concatenate inventory_type and barcode,
-                # e.g., "Extraction-ePR_L01_21w_0001" or "Filter-ePR_L01_21w_0001"
-                self.barcode_slug = '{type}-{barcode}'.format(type=slugify(self.freezer_inventory_type),
-                                                              barcode=slugify(self.extraction.barcode_slug))
-            elif self.freezer_inventory_type == InvTypes.FILTER:
-
-                # concatenate inventory_type and barcode,
-                # e.g., "Extraction-ePR_L01_21w_0001" or "Filter-ePR_L01_21w_0001"
-                self.barcode_slug = '{type}-{barcode}'.format(type=slugify(self.freezer_inventory_type),
-                                                              barcode=slugify(self.field_sample.barcode_slug))
+        if self.freezer_inventory_type == InvTypes.EXTRACTION:
+            # concatenate inventory_type and barcode,
+            # e.g., "Extraction-ePR_L01_21w_0001"
+            self.barcode_slug = '{type}-{barcode}'.format(type=slugify(self.get_freezer_inventory_type_display()),
+                                                          barcode=slugify(self.extraction.barcode_slug))
+        elif self.freezer_inventory_type == InvTypes.FILTER:
+            # concatenate inventory_type and barcode,
+            # e.g., "Filter-ePR_L01_21w_0001"
+            self.barcode_slug = '{type}-{barcode}'.format(type=slugify(self.get_freezer_inventory_type_display()),
+                                                          barcode=slugify(self.field_sample.barcode_slug))
 
         # all done, time to save changes to the db
         super(FreezerInventory, self).save(*args, **kwargs)
 
     def __str__(self):
-        return '{barcode_slug} [{row}, {column}]'.format(barcode_slug=self.barcode_slug,
-                                                         row=self.freezer_inventory_row,
-                                                         column=self.freezer_inventory_column)
+        return '{barcode_slug} [r{row}, c{column}]'.format(barcode_slug=self.barcode_slug,
+                                                           row=self.freezer_inventory_row,
+                                                           column=self.freezer_inventory_column)
 
 
 class FreezerCheckout(DateTimeUserMixin):
     freezer_inventory = models.ForeignKey(FreezerInventory, on_delete=models.RESTRICT)
-    # satisfied by "created_by" from DateTimeUserMixin
-    # freezer_user = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user), default=get_default_user)
+    # freezer_user satisfied by "created_by" from DateTimeUserMixin
     freezer_checkout_action = models.IntegerField("Freezer Checkout Action", choices=CheckoutActions.choices)
     freezer_checkout_datetime = models.DateTimeField("Freezer Checkout DateTime", blank=True, null=True)
     freezer_return_datetime = models.DateTimeField("Freezer Return DateTime", blank=True, null=True)
@@ -104,24 +127,14 @@ class FreezerCheckout(DateTimeUserMixin):
                                                    blank=True, null=True)
     freezer_return_vol_units = models.IntegerField("Volume Units", choices=VolUnits.choices, blank=True, null=True)
     freezer_return_notes = models.TextField("Return Notes", blank=True)
-    #freezer_return_action
     freezer_perm_removal_datetime = models.DateTimeField("Freezer Permanent Removal DateTime", blank=True, null=True)
 
-    def freezer_inv_status_update(self, inv_pk, freezer_checkout_action):
-        if freezer_checkout_action == 0:
-            # Checkout, so change inventory status to Checked Out
-            FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=1)
-        elif freezer_checkout_action == 1:
-            # Return, so change inventory status to In Stock
-            FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=0)
-        elif freezer_checkout_action == 2:
-            # Removed, so change inventory status to Removed
-            FreezerInventory.objects.filter(pk=inv_pk).update(freezer_inventory_status=2)
-
     def __str__(self):
-        return '{field_sample} {extraction} {pooled_library}'.format(field_sample=self.freezer_inventory.field_sample,
-                                                                     extraction=self.freezer_inventory.extraction,
-                                                                     pooled_library=self.freezer_inventory.pooled_library)
+        return '{created_datetime} ' \
+               '{checkout_action} ' \
+               '{barcode}'.format(created_datetime=self.created_datetime,
+                                  checkout_action=self.get_freezer_checkout_action_display(),
+                                  barcode=self.freezer_inventory.barcode_slug)
 
     def save(self, *args, **kwargs):
         if self.freezer_checkout_action == CheckoutActions.CHECKOUT:
@@ -133,7 +146,7 @@ class FreezerCheckout(DateTimeUserMixin):
         elif self.freezer_checkout_action == CheckoutActions.REMOVE:
             self.freezer_remove_datetime = datetime.datetime.now()
 
-        self.freezer_inv_status_update(self.freezer_inventory.pk, self.freezer_checkout_action)
+        freezer_inv_status_update(self.freezer_inventory.pk, self.freezer_checkout_action)
 
         # all done, time to save changes to the db
         super(FreezerCheckout, self).save(*args, **kwargs)
