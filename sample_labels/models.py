cefd3a81d34374feb21dc73a75c11ca443eb3fc9
Mel <mkimble88@gmail.com>
2021-08-11 12:07:23 -0400

    moved insert_update_sample_id_req def outside of class and updated __str__ methods

--- sample_labels/models.py
+++ sample_labels/models.py
@@ -8,8 +8,45 @@ from users.models import DateTimeUserMixin
 from django.core.validators import MinValueValidator
 import numpy as np
 
+
 def current_year():
     return datetime.date.today().year
+
+
+def insert_update_sample_id_req(min_sample_label_id, max_sample_label_id, min_sample_label_num,
+                                max_sample_label_num, sample_label_prefix, site_id, sample_type, sample_year,
+                                purpose):
+    if min_sample_label_id == max_sample_label_id:
+        # only one label request, so min and max label id will be the same; only need to enter
+        # one new label into SampleLabel
+        sample_label_id = min_sample_label_id
+        SampleLabel.objects.update_or_create(
+            sample_label_id=sample_label_id,
+            site_id=site_id,
+            sample_type=sample_type,
+            sample_year=sample_year,
+            purpose=purpose
+        )
+    else:
+        # more than one label requested, so need to interate to insert into SampleLabel
+        # arrange does not include max value, hence max+1
+        for num in np.arange(min_sample_label_num, max_sample_label_num + 1, 1):
+            # add leading zeros to site_num, e.g., 1 to 01
+            num_leading_zeros = str(num).zfill(4)
+
+            # format site_id, e.g., "eAL_L01"
+            sample_label_id = '{labelprefix}_{sitenum}'.format(labelprefix=sample_label_prefix,
+                                                               sitenum=num_leading_zeros)
+            # enter each new label into SampleLabel - request only has a single row with the requested
+            # number and min/max; this table is necessary for joining proceeding tables
+            SampleLabel.objects.update_or_create(
+                sample_label_id=sample_label_id,
+                site_id=site_id,
+                sample_type=sample_type,
+                sample_year=sample_year,
+                purpose=purpose
+            )
+
 
 class SampleType(DateTimeUserMixin):
     sample_type_code = models.CharField("System Code", max_length=1, unique=True)
@@ -17,6 +54,7 @@ class SampleType(DateTimeUserMixin):
 
     def __str__(self):
         return '{code}: {label}'.format(code=self.sample_type_code, label=self.sample_type_label)
+
 
 class SampleLabelRequest(DateTimeUserMixin):
     # With RESTRICT, if project is deleted but system and region still exists, it will not cascade delete
@@ -33,41 +71,8 @@ class SampleLabelRequest(DateTimeUserMixin):
     max_sample_label_id = models.CharField("Max Sample Label ID", max_length=16)
 
     def __str__(self):
-        return self.max_sample_label_id
+        return '{label}'.format(label=self.max_sample_label_id)
 
-    def insert_update_sample_id_req(self, min_sample_label_id, max_sample_label_id, min_sample_label_num,
-                                    max_sample_label_num, sample_label_prefix, site_id, sample_type, sample_year,
-                                    purpose):
-        if min_sample_label_id == max_sample_label_id:
-            # only one label request, so min and max label id will be the same; only need to enter
-            # one new label into SampleLabel
-            sample_label_id = min_sample_label_id
-            SampleLabel.objects.update_or_create(
-                sample_label_id=sample_label_id,
-                site_id=site_id,
-                sample_type=sample_type,
-                sample_year=sample_year,
-                purpose=purpose
-            )
-        else:
-            # more than one label requested, so need to interate to insert into SampleLabel
-            # arange does not include max value, hence max+1
-            for num in np.arange(min_sample_label_num, max_sample_label_num + 1, 1):
-                # add leading zeros to site_num, e.g., 1 to 01
-                num_leading_zeros = str(num).zfill(4)
-
-                # format site_id, e.g., "eAL_L01"
-                sample_label_id = '{labelprefix}_{sitenum}'.format(labelprefix=sample_label_prefix,
-                                                                   sitenum=num_leading_zeros)
-                # enter each new label into SampleLabel - request only has a single row with the requested
-                # number and min/max; this table is necessary for joining proceeding tables
-                SampleLabel.objects.update_or_create(
-                    sample_label_id=sample_label_id,
-                    site_id=site_id,
-                    sample_type=sample_type,
-                    sample_year=sample_year,
-                    purpose=purpose
-                )
     def save(self, *args, **kwargs):
         # if it already exists we don't want to change the site_id; we only want to update the associated fields.
         if self.pk is None:
@@ -95,12 +100,13 @@ class SampleLabelRequest(DateTimeUserMixin):
             # format site_id, e.g., "eAL_L01"
             self.min_sample_label_id = '{labelprefix}_{sitenum}'.format(labelprefix=self.sample_label_prefix, sitenum=min_num_leading_zeros)
             self.max_sample_label_id = '{labelprefix}_{sitenum}'.format(labelprefix=self.sample_label_prefix, sitenum=max_num_leading_zeros)
-            self.insert_update_sample_id_req(self.min_sample_label_id, self.max_sample_label_id,
-                                             self.min_sample_label_num, self.max_sample_label_num,
-                                             self.sample_label_prefix, self.site_id,
-                                             self.sample_type, self.sample_year, self.purpose)
+            insert_update_sample_id_req(self.min_sample_label_id, self.max_sample_label_id,
+                                        self.min_sample_label_num, self.max_sample_label_num,
+                                        self.sample_label_prefix, self.site_id,
+                                        self.sample_type, self.sample_year, self.purpose)
         # all done, time to save changes to the db
         super(SampleLabelRequest, self).save(*args, **kwargs)
+
 
 class SampleLabel(DateTimeUserMixin):
     sample_label_id = models.CharField("Sample Label ID", max_length=16, unique=True)
@@ -112,4 +118,4 @@ class SampleLabel(DateTimeUserMixin):
     purpose = models.CharField("Sample Label Purpose", max_length=200)
 
     def __str__(self):
-        return self.sample_label_id
+        return '{label}'.format(label=self.sample_label_id)
