83a1bd2d795e2a4890b1213a3ccd21e4dc64d595
Mel <mkimble88@gmail.com>
2021-08-11 15:07:18 -0400

    Added description to created_by, modified_datetime, and created_datetime fields

--- users/models.py
+++ users/models.py
@@ -37,9 +37,9 @@ class CustomUser(AbstractUser):
 
 class DateTimeUserMixin(models.Model):
     # these are django fields for when the record was created and by whom
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user), default=get_default_user)
-    modified_datetime = models.DateTimeField(auto_now_add=True)
-    created_datetime = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey("Created By", get_user_model(), on_delete=models.SET(get_sentinel_user), default=get_default_user)
+    modified_datetime = models.DateTimeField("Modified DateTime", auto_now_add=True)
+    created_datetime = models.DateTimeField("Created DateTime", auto_now=True)
 
     def was_added_recently(self):
         now = timezone.now()
