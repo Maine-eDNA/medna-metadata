2a793aea32d84503f4c4f814c1448420392036bc
Mel <mkimble88@gmail.com>
2021-08-11 23:01:57 -0400

    added biome and feature to fieldsiteserializer

--- field_sites/serializers.py
+++ field_sites/serializers.py
@@ -7,25 +7,33 @@ from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 # Django REST Framework to allow the automatic downloading of data!
 class EnvoBiomeSerializer(serializers.ModelSerializer):
-    biome_first_label = serializers.CharField()
-    biome_second_label = serializers.CharField()
-    biome_third_label = serializers.CharField()
-    biome_fourth_label = serializers.CharField()
-    biome_fifth_label = serializers.CharField()
+    biome_first_label = serializers.CharField(max_length=255)
+    biome_first_code = serializers.CharField(max_length=255)
 
-    biome_first_code = serializers.CharField()
-    biome_second_code = serializers.CharField()
-    biome_third_code = serializers.CharField()
-    biome_fourth_code = serializers.CharField()
-    biome_fifth_code = serializers.CharField()
+    biome_second_label = serializers.CharField(max_length=255, allow_blank=True)
+    biome_second_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    biome_third_label = serializers.CharField(max_length=255, allow_blank=True)
+    biome_third_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    biome_fourth_label = serializers.CharField(max_length=255, allow_blank=True)
+    biome_fourth_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    biome_fifth_label = serializers.CharField(max_length=255, allow_blank=True)
+    biome_fifth_code = serializers.CharField(max_length=255, allow_blank=True)
+
     created_datetime = serializers.DateTimeField()
 
     ontology_url = serializers.URLField()
 
     class Meta:
         model = EnvoBiomeFifth
-        fields = ['id', 'biome_first_label', 'biome_second_label', 'biome_third_label', 'biome_fourth_label',
-                  'biome_fifth_label', 'ontology_url', 'created_by', 'created_datetime']
+        fields = ['id', 'biome_first_label', 'biome_first_code',
+                  'biome_second_label', 'biome_second_code',
+                  'biome_third_label', 'biome_third_code',
+                  'biome_fourth_label', 'biome_fourth_code',
+                  'biome_fifth_label', 'biome_fifth_code',
+                  'ontology_url', 'created_by', 'created_datetime']
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligible field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
@@ -33,29 +41,41 @@ class EnvoBiomeSerializer(serializers.ModelSerializer):
 
 
 class EnvoFeatureSerializer(serializers.ModelSerializer):
-    feature_first_label = serializers.CharField()
-    feature_second_label = serializers.CharField()
-    feature_third_label = serializers.CharField()
-    feature_fourth_label = serializers.CharField()
-    feature_fifth_label = serializers.CharField()
-    feature_sixth_label = serializers.CharField()
-    feature_seventh_label = serializers.CharField()
+    feature_first_label = serializers.CharField(max_length=255)
+    feature_first_code = serializers.CharField(max_length=255)
 
-    feature_first_code = serializers.CharField()
-    feature_second_code = serializers.CharField()
-    feature_third_code = serializers.CharField()
-    feature_fourth_code = serializers.CharField()
-    feature_fifth_code = serializers.CharField()
-    feature_sixth_code = serializers.CharField()
-    feature_seventh_code = serializers.CharField()
+    feature_second_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_second_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    feature_third_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_third_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    feature_fourth_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_fourth_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    feature_fifth_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_fifth_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    feature_sixth_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_sixth_code = serializers.CharField(max_length=255, allow_blank=True)
+
+    feature_seventh_label = serializers.CharField(max_length=255, allow_blank=True)
+    feature_seventh_code = serializers.CharField(max_length=255, allow_blank=True)
+
     created_datetime = serializers.DateTimeField()
 
     ontology_url = serializers.URLField()
 
     class Meta:
         model = EnvoFeatureSeventh
-        fields = ['id', 'feature_first_label', 'feature_second_label', 'feature_third_label', 'feature_fourth_label',
-                  'feature_fifth_label', 'feature_sixth_label', 'feature_seventh_label', 'ontology_url',
+        fields = ['id', 'feature_first_label', 'feature_second_code',
+                  'feature_second_label', 'feature_second_code',
+                  'feature_third_label', 'feature_third_code',
+                  'feature_fourth_label', 'feature_fourth_code',
+                  'feature_fifth_label', 'feature_fifth_code',
+                  'feature_sixth_label', 'feature_sixth_code',
+                  'feature_seventh_label', 'feature_seventh_code',
+                  'ontology_url',
                   'created_by', 'created_datetime']
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligible field (like pk 1), have to add
@@ -64,8 +84,8 @@ class EnvoFeatureSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    project_code = serializers.CharField()
-    project_label = serializers.CharField()
+    project_code = serializers.CharField(max_length=1)
+    project_label = serializers.CharField(max_length=255)
     created_datetime = serializers.DateTimeField()
 
     class Meta:
@@ -78,8 +98,8 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class SystemSerializer(serializers.ModelSerializer):
-    system_code = serializers.CharField()
-    system_label = serializers.CharField()
+    system_code = serializers.CharField(max_length=1)
+    system_label = serializers.CharField(max_length=255)
     created_datetime = serializers.DateTimeField()
 
     class Meta:
@@ -92,8 +112,9 @@ class SystemSerializer(serializers.ModelSerializer):
 
 
 class FieldSiteSerializer(serializers.ModelSerializer):
-    lat = serializers.DecimalField()
-    lon = serializers.DecimalField()
+    lat = serializers.DecimalField(max_digits=22, decimal_places=16)
+    lon = serializers.DecimalField(max_digits=22, decimal_places=16)
+    site_id = serializers.CharField(max_length=7)
     srid = serializers.CharField()
     created_datetime = serializers.DateTimeField()
 
@@ -107,7 +128,22 @@ class FieldSiteSerializer(serializers.ModelSerializer):
     project = serializers.SlugRelatedField(many=False, read_only=True, slug_field='project_label')
     system = serializers.SlugRelatedField(many=False, read_only=True, slug_field='system_label')
     region = serializers.SlugRelatedField(many=False, read_only=True, slug_field='region_label')
+    # ENVO biomes are hierarchical trees
+    envo_biome_first = serializers.SlugRelatedField(many=False, read_only=True, slug_field="biome_first_label")
+    envo_biome_second = serializers.SlugRelatedField(many=False, read_only=True, slug_field="biome_second_label")
+    envo_biome_third = serializers.SlugRelatedField(many=False, read_only=True, slug_field="biome_third_label")
+    envo_biome_fourth = serializers.SlugRelatedField(many=False, read_only=True, slug_field="biome_fourth_label")
+    envo_biome_fifth = serializers.SlugRelatedField(many=False, read_only=True, slug_field="biome_fifth_label")
+    # ENVO Features are hierarchical trees
+    envo_feature_first = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_first_label")
+    envo_feature_second = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_second_label")
+    envo_feature_third = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_third_label")
+    envo_feature_fourth = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_fourth_label")
+    envo_feature_fifth = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_fifth_label")
+    envo_feature_sixth = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_sixth_label")
+    envo_feature_seventh = serializers.SlugRelatedField(many=False, read_only=True, slug_field="feature_seventh_label")
     created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='email')
+
 
 
 class GeoFieldSiteSerializer(GeoFeatureModelSerializer):
