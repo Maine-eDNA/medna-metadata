f6253c6014d97b3a5fd1a3d692e9807bd71d74a7
Mel <mkimble88@gmail.com>
2021-08-11 21:24:43 -0400

    added viewsets to reflect all bioinfo_denoising models

--- bioinfo_denoising/views.py
+++ bioinfo_denoising/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .serializers import DenoisingMethodSerializer, DenoisingMetadataSerializer, \
+    AmpliconSequenceVariantSerializer, ASVReadSerializer
+from .models import DenoisingMethod, DenoisingMetadata, AmpliconSequenceVariant, ASVRead
+from rest_framework import viewsets
 
-# Create your views here.
+
+class DenoisingMethodViewSet(viewsets.ModelViewSet):
+    serializer_class = DenoisingMethodSerializer
+    queryset = DenoisingMethod.objects.all()
+
+
+class DenoisingMetadataViewSet(viewsets.ModelViewSet):
+    serializer_class = DenoisingMetadataSerializer
+    queryset = DenoisingMetadata.objects.all()
+
+
+class AmpliconSequenceVariantViewSet(viewsets.ModelViewSet):
+    serializer_class = AmpliconSequenceVariantSerializer
+    queryset = AmpliconSequenceVariant.objects.all()
+
+
+class ASVReadViewSet(viewsets.ModelViewSet):
+    serializer_class = ASVReadSerializer
+    queryset = ASVRead.objects.all()
