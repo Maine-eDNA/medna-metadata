8757b7faed3ce5ec60f39fd62c2d31d4c50211b8
Mel <mkimble88@gmail.com>
2021-08-11 22:48:45 -0400

    fixed style formatting

--- field_survey/serializers.py
+++ field_survey/serializers.py
@@ -31,18 +31,18 @@ class FieldSurveySerializer(serializers.ModelSerializer):
     env_notes = serializers.CharField()
     env_measure_mode = serializers.CharField()
     env_boat_type = serializers.CharField()
-    env_bottom_depth = serializers.FloatField()
+    env_bottom_depth = serializers.DecimalField()
     measurements_taken = serializers.ChoiceField(choices=YesNo.choices)
     survey_complete = serializers.ChoiceField(choices=YesNo.choices)
     qa_datetime = serializers.DateTimeField()
     qa_initial = serializers.CharField()
     gps_cap_lat = serializers.DecimalField()
     gps_cap_long = serializers.DecimalField()
-    gps_cap_alt = serializers.FloatField()
-    gps_cap_horiz_acc = serializers.FloatField()
-    gps_cap_vert_acc = serializers.FloatField()
-    record_create_date = serializers.DateTimeField()
-    record_edit_date = serializers.DateTimeField()
+    gps_cap_alt = serializers.DecimalField()
+    gps_cap_horiz_acc = serializers.DecimalField()
+    gps_cap_vert_acc = serializers.DecimalField()
+    record_create_datetime = serializers.DateTimeField()
+    record_edit_datetime = serializers.DateTimeField()
     created_datetime = serializers.DateTimeField()
 
     class Meta:
@@ -53,8 +53,8 @@ class FieldSurveySerializer(serializers.ModelSerializer):
                   'env_biome_other', 'env_feature', 'env_feature_other', 'env_material', 'env_material_other',
                   'env_notes', 'env_measure_mode', 'env_boat_type', 'env_bottom_depth', 'measurements_taken',
                   'survey_complete', 'qa_datetime', 'qa_initial', 'gps_cap_lat', 'gps_cap_long', 'gps_cap_alt',
-                  'gps_cap_horiz_acc', 'gps_cap_vert_acc', 'record_create_date', 'record_edit_date',
-                  'created_by', 'created_datetime',]
+                  'gps_cap_horiz_acc', 'gps_cap_vert_acc', 'record_create_datetime', 'record_edit_datetime',
+                  'created_by', 'created_datetime', ]
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligable field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
@@ -77,7 +77,8 @@ class FieldCrewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = FieldCrew
-        fields = ['crew_global_id', 'crew_fname', 'crew_lname', 'survey_global_id', 'created_by', 'created_datetime',]
+        fields = ['crew_global_id', 'crew_fname', 'crew_lname', 'survey_global_id',
+                  'created_by', 'created_datetime', ]
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligable field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
@@ -88,7 +89,7 @@ class FieldCrewSerializer(serializers.ModelSerializer):
 class EnvMeasurementSerializer(serializers.ModelSerializer):
     env_global_id = serializers.CharField()
     env_measure_datetime = serializers.DateTimeField()
-    env_measure_depth = serializers.FloatField()
+    env_measure_depth = serializers.DecimalField()
     env_instrument = serializers.CharField()
     # env_ctd_fname
     env_ctd_filename = serializers.CharField()
@@ -98,28 +99,28 @@ class EnvMeasurementSerializer(serializers.ModelSerializer):
     env_ysi_model = serializers.CharField()
     env_ysi_sn = serializers.CharField()
     env_ysi_notes = serializers.CharField()
-    env_secchi_depth = serializers.FloatField()
+    env_secchi_depth = serializers.DecimalField()
     env_secchi_notes = serializers.CharField()
     env_niskin_number = serializers.IntegerField()
     env_niskin_notes = serializers.CharField()
     env_inst_other = serializers.CharField()
     env_measurement = serializers.CharField()
-    env_flow_rate = serializers.FloatField()
-    env_water_temp = serializers.FloatField()
+    env_flow_rate = serializers.DecimalField()
+    env_water_temp = serializers.DecimalField()
     # env_sal
-    env_salinity = serializers.FloatField()
-    env_ph_scale = serializers.FloatField()
-    env_par1 = serializers.FloatField()
-    env_par2 = serializers.FloatField()
-    env_turbidity = serializers.FloatField()
-    env_conductivity = serializers.FloatField()
-    env_do = serializers.FloatField()
-    env_pheophytin = serializers.FloatField()
-    env_chla = serializers.FloatField()
-    env_no3no2 = serializers.FloatField()
-    env_no2 = serializers.FloatField()
-    env_nh4 = serializers.FloatField()
-    env_phosphate = serializers.FloatField()
+    env_salinity = serializers.DecimalField()
+    env_ph_scale = serializers.DecimalField()
+    env_par1 = serializers.DecimalField()
+    env_par2 = serializers.DecimalField()
+    env_turbidity = serializers.DecimalField()
+    env_conductivity = serializers.DecimalField()
+    env_do = serializers.DecimalField()
+    env_pheophytin = serializers.DecimalField()
+    env_chla = serializers.DecimalField()
+    env_no3no2 = serializers.DecimalField()
+    env_no2 = serializers.DecimalField()
+    env_nh4 = serializers.DecimalField()
+    env_phosphate = serializers.DecimalField()
     env_substrate = serializers.CharField()
     env_lab_datetime = serializers.DateTimeField()
     env_measure_notes = serializers.CharField()
@@ -133,7 +134,7 @@ class EnvMeasurementSerializer(serializers.ModelSerializer):
                   'env_measurement', 'env_flow_rate', 'env_water_temp', 'env_salinity', 'env_ph_scale', 'env_par1',
                   'env_par2', 'env_turbidity', 'env_conductivity', 'env_do', 'env_pheophytin', 'env_chla', 'env_no3no2',
                   'env_no2', 'env_nh4', 'env_phosphate', 'env_substrate', 'env_lab_datetime', 'env_measure_notes',
-                  'created_datetime',]
+                  'created_by', 'created_datetime', ]
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligable field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
@@ -148,11 +149,11 @@ class FieldCollectionSerializer(serializers.ModelSerializer):
     water_control_type = serializers.CharField()
     water_vessel_label = serializers.CharField()
     water_collect_datetime = serializers.DateTimeField()
-    water_collect_depth = serializers.FloatField()
+    water_collect_depth = serializers.DecimalField()
     water_collect_mode = serializers.CharField()
     water_niskin_number = serializers.IntegerField()
-    water_niskin_vol = serializers.FloatField()
-    water_vessel_vol = serializers.FloatField()
+    water_niskin_vol = serializers.DecimalField()
+    water_vessel_vol = serializers.DecimalField()
     water_vessel_material = serializers.CharField()
     water_vessel_color = serializers.CharField()
     water_collect_notes = serializers.CharField()
@@ -163,9 +164,9 @@ class FieldCollectionSerializer(serializers.ModelSerializer):
     core_datetime_end = serializers.DateTimeField()
     core_method = serializers.CharField()
     core_method_other = serializers.CharField()
-    core_collect_depth = serializers.FloatField()
-    core_length = serializers.FloatField()
-    core_diameter = serializers.FloatField()
+    core_collect_depth = serializers.DecimalField()
+    core_length = serializers.DecimalField()
+    core_diameter = serializers.DecimalField()
     core_purpose = serializers.CharField()
     core_notes = serializers.CharField()
     subcores_taken = serializers.ChoiceField(choices=YesNo.choices)
@@ -178,8 +179,8 @@ class FieldCollectionSerializer(serializers.ModelSerializer):
                   'water_niskin_number', 'water_niskin_vol', 'water_vessel_vol', 'water_vessel_material',
                   'water_vessel_color', 'water_collect_notes', 'was_filtered', 'core_control', 'core_label',
                   'core_datetime_start', 'core_datetime_end', 'core_method', 'core_method_other', 'core_collect_depth',
-                  'core_length', 'core_diameter', 'core_purpose', 'core_notes', 'subcores_taken', 'created_by',
-                  'created_datetime']
+                  'core_length', 'core_diameter', 'core_purpose', 'core_notes', 'subcores_taken',
+                  'created_by', 'created_datetime', ]
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligable field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
@@ -198,7 +199,7 @@ class FieldSampleSerializer(serializers.ModelSerializer):
     filter_datetime = serializers.DateTimeField()
     filter_method = serializers.CharField()
     filter_method_other = serializers.CharField()
-    filter_vol = serializers.FloatField()
+    filter_vol = serializers.DecimalField()
     filter_type = serializers.CharField()
     filter_type_other = serializers.CharField()
     filter_pore = serializers.IntegerField()
@@ -211,15 +212,16 @@ class FieldSampleSerializer(serializers.ModelSerializer):
     subcore_datetime_start = serializers.DateTimeField()
     subcore_datetime_end = serializers.DateTimeField()
     subcore_number = serializers.IntegerField()
-    subcore_length = serializers.FloatField()
-    subcore_diameter = serializers.FloatField()
+    subcore_length = serializers.DecimalField()
+    subcore_diameter = serializers.DecimalField()
     subcore_clayer = serializers.IntegerField()
     created_datetime = serializers.DateTimeField()
 
     class Meta:
         model = FieldSample
         fields = ['id', 'site_id', 'project', 'system', 'region', 'general_location_name',
-                  'purpose', 'lat', 'lon', 'srid', 'created_by', 'created_datetime']
+                  'purpose', 'lat', 'lon', 'srid',
+                  'created_by', 'created_datetime', ]
     # Since project, system, region, and created_by reference different tables and we
     # want to show 'label' rather than some unintelligable field (like pk 1), have to add
     # slug to tell it to print the desired field from the other table
