939dc4134fed85d86635ff291ae7ccd127da9a6c
Mel <mkimble88@gmail.com>
2021-08-11 20:18:15 -0400

    added viewsets for all freezer_inventory models

--- freezer_inventory/views.py
+++ freezer_inventory/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from .serializers import FreezerSerializer, FreezerRackSerializer, FreezerBoxSerializer, FreezerInventorySerializer, \
+    FreezerCheckoutSerializer
+from .models import Freezer, FreezerRack, FreezerBox, FreezerInventory, FreezerCheckout
+from rest_framework import viewsets
 # Create your views here.
+
+
+class FreezerViewSet(viewsets.ModelViewSet):
+    serializer_class = FreezerSerializer
+    queryset = Freezer.objects.all()
+
+
+class FreezerRackViewSet(viewsets.ModelViewSet):
+    serializer_class = FreezerRackSerializer
+    queryset = FreezerRack.objects.all()
+
+
+class FreezerBoxViewSet(viewsets.ModelViewSet):
+    serializer_class = FreezerBoxSerializer
+    queryset = FreezerBox.objects.all()
+
+
+class FreezerInventoryViewSet(viewsets.ModelViewSet):
+    serializer_class = FreezerInventorySerializer
+    queryset = FreezerInventory.objects.all()
+
+
+class FreezerCheckoutViewSet(viewsets.ModelViewSet):
+    serializer_class = FreezerCheckoutSerializer
+    queryset = FreezerCheckout.objects.all()
