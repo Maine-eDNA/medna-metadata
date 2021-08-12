7b79d71662ef4624cc64daf7700618ae73237ca6
Mel <mkimble88@gmail.com>
2021-08-11 22:07:42 -0400

    Fixed style formatting

--- bioinfo_taxon/models.py
+++ bioinfo_taxon/models.py
@@ -144,7 +144,8 @@ class TaxonomicAnnotation(DateTimeUserMixin):
     asv = models.ForeignKey(AmpliconSequenceVariant, on_delete=models.RESTRICT)
     annotation_metadata = models.ForeignKey(AnnotationMetadata, on_delete=models.RESTRICT)
     reference_database = models.ForeignKey(ReferenceDatabase, on_delete=models.RESTRICT)
-    confidence = models.DecimalField("Confidence", max_digits=10, decimal_places=10, blank=True, null=True)
+    confidence = models.DecimalField("Confidence", max_digits=10, decimal_places=10,
+                                     blank=True, null=True)
     ta_taxon = models.TextField("Taxon", blank=True)
     ta_domain = models.CharField("Domain", max_length=255, blank=True)
     ta_kingdom = models.CharField("Kingdom", max_length=255, blank=True)
