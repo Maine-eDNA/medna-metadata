fa6836751f8b291c9a9ac472ccc5f9a003e6129d
Mel <mkimble88@gmail.com>
2021-08-13 01:55:39 -0400

    added samplelabel model to admin and resources

--- sample_labels/admin.py
+++ sample_labels/admin.py
@@ -1,9 +1,10 @@
 # Register your models here.
 from django.contrib import admin
-from .models import SampleType, SampleLabelRequest
+from .models import SampleType, SampleLabelRequest, SampleLabel
 from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter
 from import_export.admin import ImportExportActionModelAdmin, ExportActionModelAdmin
-from .resources import SampleLabelRequestAdminResource, SampleTypeAdminResource
+from .resources import SampleLabelRequestAdminResource, SampleTypeAdminResource, \
+    SampleLabelAdminResource
 
 
 class SampleTypeAdmin(ImportExportActionModelAdmin):
@@ -73,3 +74,41 @@ class SampleLabelRequestAdmin(ExportActionModelAdmin):
 
 
 admin.site.register(SampleLabelRequest, SampleLabelRequestAdmin)
+
+
+class SampleLabelAdmin(ExportActionModelAdmin):
+    # below are import_export configs
+    resource_class = SampleLabelAdminResource
+    # changes the order of how the tables are displayed and specifies what to display
+    list_display = ('__str__',)
+
+    def add_view(self, request, extra_content=None):
+        # specify the fields that can be viewed in add view
+        self.fields = ['sample_label_id', 'site_id', 'sample_type', 'sample_year', 'purpose',
+                       'created_by']
+        self.list_filter = (
+            ('sample_type', RelatedDropdownFilter),
+            ('site_id', RelatedDropdownFilter),
+        )
+        # self.exclude = ('site_prefix', 'site_num','site_id','created_datetime')
+        add_fields = request.GET.copy()
+        add_fields['created_by'] = request.user
+        request.GET = add_fields
+        return super(SampleLabelAdmin, self).add_view(request)
+
+    def change_view(self, request, object_id, extra_content=None):
+        # specify what can be changed in admin change view
+        self.fields = ['purpose', 'created_by']
+        # self.exclude = ('site_prefix', 'site_num','site_id','created_datetime')
+        return super(SampleLabelAdmin, self).change_view(request, object_id)
+
+    # removes "delete selected" from drop down menu
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+    # below are import_export configs
+
+
+admin.site.register(SampleLabel, SampleLabelAdmin)
--- sample_labels/resources.py
+++ sample_labels/resources.py
@@ -1,6 +1,6 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
-from .models import SampleLabelRequest, SampleType
+from .models import SampleLabelRequest, SampleLabel, SampleType
 from field_sites.models import FieldSite
 from users.models import CustomUser
 
@@ -23,6 +23,27 @@ class SampleLabelRequestAdminResource(resources.ModelResource):
         export_order = ('sample_label_prefix', 'req_sample_label_num', 'min_sample_label_num',
                         'max_sample_label_num', 'min_sample_label_id', 'max_sample_label_id', 'site_id',
                         'sample_year', 'sample_type', 'purpose', 'created_by', 'created_datetime',)
+    site_id = fields.Field(
+        column_name='site_id',
+        attribute='site_id',
+        widget=ForeignKeyWidget(FieldSite, 'site_id'))
+    sample_type = fields.Field(
+        column_name='sample_type',
+        attribute='sample_type',
+        widget=ForeignKeyWidget(SampleType, 'sample_type_label'))
+    created_by = fields.Field(
+        column_name='created_by',
+        attribute='created_by',
+        widget=ForeignKeyWidget(CustomUser, 'email'))
+
+
+class SampleLabelAdminResource(resources.ModelResource):
+    class Meta:
+        model = SampleLabel
+        fields = ('sample_label_id', 'site_id', 'sample_type', 'sample_year', 'purpose',
+                  'created_by', 'created_datetime', )
+        export_order = ('sample_label_id', 'site_id', 'sample_type', 'sample_year', 'purpose',
+                        'created_by', 'created_datetime', )
     site_id = fields.Field(
         column_name='site_id',
         attribute='site_id',
