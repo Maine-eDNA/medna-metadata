7fe0d361c19a9ff5a386cc6ad7e8fd2ecd212263
Mel <mkimble88@gmail.com>
2021-08-18 00:15:13 -0400

    Removed all enum from field_survey

--- field_survey/models.py
+++ field_survey/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from sample_labels.models import SampleLabel, SampleType
 from field_sites.models import FieldSite
 from users.models import DateTimeUserMixin, get_sentinel_user
-from users.enumerations import YesNo
 from django.utils.text import slugify
 
 
@@ -58,7 +57,7 @@ class FieldSurvey(DateTimeUserMixin):
     env_measure_mode = models.CharField("Collection Mode", max_length=255, blank=True)
     env_boat_type = models.CharField("Boat Type", max_length=255, blank=True)
     env_bottom_depth = models.DecimalField("Bottom Depth (m)", max_digits=15, decimal_places=10, blank=True, null=True)
-    measurements_taken = models.IntegerField("Measurements Taken", choices=YesNo.choices, blank=True, null=True)
+    measurements_taken = models.CharField("Measurements Taken", max_length=3, blank=True)
     core_subcorer = models.ForeignKey(settings.AUTH_USER_MODEL,
                                       verbose_name="Designated Sub-corer",
                                       blank=True, null=True,
@@ -69,7 +68,7 @@ class FieldSurvey(DateTimeUserMixin):
                                        blank=True, null=True,
                                        on_delete=models.SET(get_sentinel_user),
                                        related_name="water_filterer")
-    survey_complete = models.IntegerField("Survey Complete", choices=YesNo.choices, blank=True, null=True)
+    survey_complete = models.CharField("Survey Complete", max_length=3, blank=True)
     qa_editor = models.ForeignKey(settings.AUTH_USER_MODEL,
                                   verbose_name="Quality Editor",
                                   blank=True, null=True,
@@ -240,7 +239,7 @@ class FieldCollection(DateTimeUserMixin):
                                          related_name="fieldsurvey_to_fieldcollection",
                                          on_delete=models.CASCADE)
     collection_type = models.CharField("Collection Type (water or sediment)", max_length=255, blank=True)
-    water_control = models.IntegerField("Is Control", choices=YesNo.choices, blank=True, null=True)
+    water_control = models.CharField("Is Control", max_length=3, blank=True)
     water_control_type = models.CharField("Water Control Type", max_length=255, blank=True)
     water_vessel_label = models.TextField("Water Vessel Label", blank=True)
     water_collect_datetime = models.DateTimeField("Water Collection DateTime", blank=True, null=True)
@@ -256,8 +255,8 @@ class FieldCollection(DateTimeUserMixin):
     water_vessel_color = models.CharField("Water Vessel Color", max_length=255, blank=True)
     water_collect_notes = models.TextField("Water Sample Notes", blank=True)
     # wasfiltered
-    was_filtered = models.IntegerField("Filtered", choices=YesNo.choices, blank=True, null=True)
-    core_control = models.IntegerField("Is Control", choices=YesNo.choices, blank=True, null=True)
+    was_filtered = models.CharField("Filtered", max_length=3, blank=True)
+    core_control = models.CharField("Is Control", max_length=3, blank=True)
     core_label = models.TextField("Core Label", blank=True)
     core_datetime_start = models.DateTimeField("Core Start DateTime", blank=True, null=True)
     core_datetime_end = models.DateTimeField("Core End DateTime", blank=True, null=True)
@@ -272,7 +271,7 @@ class FieldCollection(DateTimeUserMixin):
     core_purpose = models.TextField("Purpose of Other Cores", blank=True)
     core_notes = models.TextField("Core Notes", blank=True)
     # subcorestaken
-    subcores_taken = models.IntegerField("Sub-Cored", choices=YesNo.choices, blank=True, null=True)
+    subcores_taken = models.CharField("Sub-Cored", max_length=3, blank=True)
 
     def __str__(self):
         return '{survey_global_id}, ' \
@@ -296,10 +295,10 @@ class FieldSample(DateTimeUserMixin):
     field_sample_barcode = models.OneToOneField(SampleLabel, on_delete=models.RESTRICT,
                                                 unique=True)
     barcode_slug = models.SlugField(max_length=16, unique=True, null=True)
-    is_extracted = models.IntegerField("Extracted", choices=YesNo.choices, default=YesNo.NO)
+    is_extracted = models.CharField("Extracted", max_length=3, default="No")
     sample_type = models.ForeignKey(SampleType, on_delete=models.RESTRICT)
     filter_location = models.CharField("Filter Location", max_length=255, blank=True)
-    is_prefilter = models.IntegerField("Prefilter", choices=YesNo.choices, blank=True, null=True)
+    is_prefilter = models.CharField("Prefilter", max_length=3, blank=True, null=True)
     filter_fname = models.CharField("Filterer First Name", max_length=255, blank=True)
     filter_lname = models.CharField("Filterer Last Name", max_length=255, blank=True)
     filter_sample_label = models.TextField("Filter Sample Label", blank=True)
@@ -394,7 +393,7 @@ class FieldSurveyETL(DateTimeUserMixin):
     env_boat_type = models.CharField("Boat Type", max_length=255, blank=True)
     env_bottom_depth = models.DecimalField("Bottom Depth (m)",
                                            max_digits=15, decimal_places=10, blank=True, null=True)
-    measurements_taken = models.IntegerField("Measurements Taken", choices=YesNo.choices, blank=True, null=True)
+    measurements_taken = models.CharField("Measurements Taken", max_length=3, blank=True, null=True)
     core_subcorer = models.ForeignKey(settings.AUTH_USER_MODEL,
                                       verbose_name="Designated Sub-corer",
                                       blank=True,
@@ -405,7 +404,7 @@ class FieldSurveyETL(DateTimeUserMixin):
                                        blank=True,
                                        on_delete=models.SET(get_sentinel_user),
                                        related_name="water_filterer_etl")
-    survey_complete = models.IntegerField("Survey Complete", choices=YesNo.choices, blank=True, null=True)
+    survey_complete = models.CharField("Survey Complete", max_length=3, blank=True, null=True)
     qa_editor = models.ForeignKey(settings.AUTH_USER_MODEL,
                                   verbose_name="Quality Editor",
                                   blank=True,
@@ -569,7 +568,7 @@ class FieldCollectionETL(DateTimeUserMixin):
     collection_global_id = models.TextField("Global ID", primary_key=True)
     # this should be a fk to sample_labels, but I need to change the option labels in survey123 for it to work
     collection_type = models.CharField("Collection Type (water or sediment)", max_length=255, blank=True)
-    water_control = models.IntegerField("Is Control", choices=YesNo.choices, blank=True, null=True)
+    water_control = models.CharField("Is Control", max_length=3, blank=True, null=True)
     water_control_type = models.CharField("Water Control Type", max_length=255, blank=True)
     water_vessel_label = models.TextField("Water Vessel Label", blank=True)
     water_collect_datetime = models.DateTimeField("Water Collection DateTime", blank=True, null=True)
@@ -584,8 +583,8 @@ class FieldCollectionETL(DateTimeUserMixin):
     water_vessel_material = models.CharField("Water Vessel Material", max_length=255, blank=True)
     water_vessel_color = models.CharField("Water Vessel Color", max_length=255, blank=True)
     water_collect_notes = models.TextField("Water Sample Notes", blank=True)
-    was_filtered = models.IntegerField("Filtered", choices=YesNo.choices, blank=True, null=True)
-    core_control = models.IntegerField("Is Control", choices=YesNo.choices, blank=True, null=True)
+    was_filtered = models.CharField("Filtered", max_length=3, blank=True, null=True)
+    core_control = models.CharField("Is Control", max_length=3, blank=True, null=True)
     core_label = models.TextField("Core Label", blank=True)
     core_datetime_start = models.DateTimeField("Core Start DateTime", blank=True, null=True)
     core_datetime_end = models.DateTimeField("Core End DateTime", blank=True, null=True)
@@ -598,7 +597,7 @@ class FieldCollectionETL(DateTimeUserMixin):
     core_diameter = models.DecimalField("Core Diameter (cm)",
                                         max_digits=15, decimal_places=10, blank=True, null=True)
     # subcorestaken
-    subcores_taken = models.IntegerField("Sub-Cored", choices=YesNo.choices, blank=True, null=True)
+    subcores_taken = models.CharField("Sub-Cored", max_length=3, blank=True, null=True)
     subcore_fname = models.CharField("Sub-Corer First Name", max_length=255, blank=True)
     subcore_lname = models.CharField("Sub-Corer Last Name", max_length=255, blank=True)
     subcore_method = models.CharField("Sub-Core Method", max_length=255, blank=True)
@@ -641,7 +640,7 @@ class SampleFilterETL(DateTimeUserMixin):
     # id = models.AutoField(unique=True)
     filter_global_id = models.TextField("Global ID", primary_key=True)
     filter_location = models.CharField("Filter Location", max_length=255, blank=True)
-    is_prefilter = models.IntegerField("Prefilter", choices=YesNo.choices, blank=True, null=True)
+    is_prefilter = models.CharField("Prefilter", max_length=3, blank=True)
     filter_fname = models.CharField("Filterer First Name", max_length=255, blank=True)
     filter_lname = models.CharField("Filterer Last Name", max_length=255, blank=True)
     filter_sample_label = models.TextField("Filter Sample Label", blank=True)
@@ -679,7 +678,7 @@ class BlankSampleFilterETL(DateTimeUserMixin):
     # id = models.AutoField(unique=True)
     filter_global_id = models.TextField("Global ID", primary_key=True)
     filter_location = models.CharField("Filter Location", max_length=255, blank=True)
-    is_prefilter = models.IntegerField("Prefilter", choices=YesNo.choices, blank=True)
+    is_prefilter = models.CharField("Prefilter", max_length=3, blank=True)
     filter_fname = models.CharField("Filterer First Name", max_length=255, blank=True)
     filter_lname = models.CharField("Filterer Last Name", max_length=255, blank=True)
     filter_sample_label = models.TextField("Filter Sample Label", blank=True)
