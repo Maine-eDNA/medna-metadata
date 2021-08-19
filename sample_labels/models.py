30bc6fdba08b5f1760ecdb1a55ffa6e0bb2202e9
Mel <mkimble88@gmail.com>
2021-08-19 02:13:44 -0400

    removed unnecessary if statement in save method

--- sample_labels/models.py
+++ sample_labels/models.py
@@ -113,7 +113,6 @@ class SampleLabelRequest(DateTimeUserMixin):
                                         self.min_sample_label_num, self.max_sample_label_num,
                                         self.sample_label_prefix, self.site_id,
                                         self.sample_type, self.sample_year, self.purpose)
-        if self.pk is None:
             now = datetime.datetime.now()
             now_fmt = now.strftime('%Y%m%d_%H%M%S')
             self.sample_label_request_slug = '{name}_{date}'.format(name=slugify(self.sample_label_prefix),
