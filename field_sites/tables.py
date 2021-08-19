74c37a1cdb96c485aed490587d9f6275fe35d9fa
Mel <mkimble88@gmail.com>
2021-08-18 21:13:30 -0400

    Fixed style formatting

--- field_sites/tables.py
+++ field_sites/tables.py
@@ -4,11 +4,12 @@ from django_tables2.utils import A
 
 
 class FieldSiteTable(tables.Table):
-    #id = tables.CheckBoxColumn(accessor='pk')
+    # id = tables.CheckBoxColumn(accessor='pk')
     # same as <a href="{% url 'users:site_detail' site.id %}"> {{ site.site_id }}</a>
     site_id = tables.LinkColumn('users:site_detail', args=[A('pk')])
     # same as <a href="{% url 'users:site_samplelabel_add' site.id %}" class="addlink"> {% translate 'Add' %}</a>
-    add_label = tables.LinkColumn("users:site_samplelabel_add", attrs={"td": {"class": "addlink"}}, text='Add', args=[A("pk")])
+    add_label = tables.LinkColumn("users:site_samplelabel_add", attrs={"td": {"class": "addlink"}},
+                                  text='Add', args=[A("pk")])
     # formatting for date column
     created_datetime = tables.DateTimeColumn(format="M d, Y")
     _selected_action = tables.CheckBoxColumn(accessor="pk",
@@ -22,7 +23,7 @@ class FieldSiteTable(tables.Table):
         fields = ("_selected_action", "site_id", "general_location_name",
                   "grant", "system", "region", "created_datetime")
         # set table css class to "result_lust"
-        #attrs = {"class": "result_list"}
+        # attrs = {"class": "result_list"}
         # this is NOT the template it writes to, this is the template it uses to load with
         # when using the 'render_table table' tag in html
-        #template_name = "django_tables2/bootstrap.html" # now set in settings.py
+        # template_name = "django_tables2/bootstrap.html" # now set in settings.py
