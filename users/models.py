74da751f433cea8c2195ba2cb61a4e47495b041f
Mel <mkimble88@gmail.com>
2021-08-23 14:58:04 -0400

    added null=true and unique=true to agol_username field

--- users/models.py
+++ users/models.py
@@ -20,7 +20,10 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     phone_number = PhoneNumberField(blank=True, null=True)
-    agol_username = models.CharField("ArcGIS Online Username", max_length=255, blank=True)
+    # blank and null = True here so that unique can also = True even if
+    # there are blank entries elsewhere
+    agol_username = models.CharField("ArcGIS Online Username", max_length=255,
+                                     null=True, blank=True, unique=True)
     is_temporary = models.BooleanField(_('temporary status'), default=False,
                                        help_text=_('Designates whether the user is temporary.'),)
     expiration_date = models.DateTimeField("Expiration Date")
