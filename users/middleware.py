09eb384cd120625f6729037bd8e8e42181897e48
Mel <mkimble88@gmail.com>
2021-08-23 14:26:17 -0400

    added account expiration middlewear and fields

---
+++ users/middleware.py
@@ -0,0 +1,24 @@
+import requests
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+# https://docs.djangoproject.com/en/3.2/topics/http/middleware/
+
+
+# https://stackoverflow.com/questions/51188439/handling-django-user-subscription-expiry-date
+# https://stackoverflow.com/questions/12270928/best-way-to-extend-django-authentication-authorization
+# https://stackoverflow.com/questions/57235057/how-to-make-user-permissions-that-expire-after-24-hours
+class AccountExpiry:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_user = request.user
+        response = self.get_response(request)
+        expiry_path = reverse('users:account-expired')
+
+        if current_user.is_anonymous is False:
+            if current_user.admin is False and current_user.staff is False:
+                if request.path not in [expiry_path]:
+                    if current_user.is_expired is True:
+                        return HttpResponseRedirect(expiry_path)
+        return response
--- users/models.py
+++ users/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
+from medna_metadata.settings import DEFAULT_TEMP_TENURE
+import datetime
+from django.utils import timezone
 
 
 class CustomUser(AbstractUser):
@@ -17,7 +20,24 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     phone_number = PhoneNumberField(blank=True, null=True)
-    agol_username = models.CharField("ArcGIS Online Username", max_length=200, blank=True)
+    agol_username = models.CharField("ArcGIS Online Username", max_length=255, blank=True)
+    is_temporary = models.BooleanField(_('temporary status'), default=False,
+                                       help_text=_('Designates whether the user is temporary.'),)
+    expiration_date = models.DateTimeField("Expiration Date")
+
+    @property
+    def is_expired(self):
+        now = timezone.now()
+        return self.expiration_date <= now
+
+    def save(self, *args, **kwargs):
+        if not self.expiration_date:
+            if self.is_temporary:
+                now = timezone.now()
+                self.expiration_date = now + datetime.timedelta(days=DEFAULT_TEMP_TENURE)
+            else:
+                # maximum possible datetime
+                self.expiration_date = datetime.datetime.max
 
     def __str__(self):
         return self.email
