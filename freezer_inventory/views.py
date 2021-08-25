ad5deb7b8b9b8053cfe8fcffab4b73708916d615
Mel <mkimble88@gmail.com>
2021-08-25 14:06:14 -0400

    added lookup_fields to freezer_inventory serializer

--- freezer_inventory/views.py
+++ freezer_inventory/views.py
@@ -2,30 +2,36 @@ from django.shortcuts import render
 from .serializers import FreezerSerializer, FreezerRackSerializer, FreezerBoxSerializer, FreezerInventorySerializer, \
     FreezerCheckoutSerializer
 from .models import Freezer, FreezerRack, FreezerBox, FreezerInventory, FreezerCheckout
+from utility.serializers import MultipleFieldLookupMixin
 from rest_framework import viewsets
 # Create your views here.
 
 
-class FreezerViewSet(viewsets.ModelViewSet):
+class FreezerViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
     serializer_class = FreezerSerializer
     queryset = Freezer.objects.all()
+    lookup_fields = ['created_by']
 
 
-class FreezerRackViewSet(viewsets.ModelViewSet):
+class FreezerRackViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
     serializer_class = FreezerRackSerializer
     queryset = FreezerRack.objects.all()
+    lookup_fields = ['freezer', 'created_by']
 
 
-class FreezerBoxViewSet(viewsets.ModelViewSet):
+class FreezerBoxViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
     serializer_class = FreezerBoxSerializer
     queryset = FreezerBox.objects.all()
+    lookup_fields = ['freezer_rack', 'created_by']
 
 
-class FreezerInventoryViewSet(viewsets.ModelViewSet):
+class FreezerInventoryViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
     serializer_class = FreezerInventorySerializer
     queryset = FreezerInventory.objects.all()
+    lookup_fields = ['freezer_box', 'field_sample', 'extraction', 'created_by']
 
 
-class FreezerCheckoutViewSet(viewsets.ModelViewSet):
+class FreezerCheckoutViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
     serializer_class = FreezerCheckoutSerializer
     queryset = FreezerCheckout.objects.all()
+    lookup_fields = ['freezer_inventory', 'created_by']
--- utility/serializers.py
+++ utility/serializers.py
@@ -4,6 +4,25 @@ from django_tables2.export.export import TableExport
 from rest_framework import serializers
 from .models import ProcessLocation, Project, Grant
 from rest_framework.validators import UniqueValidator
+from django.shortcuts import get_object_or_404
+
+
+# https://www.django-rest-framework.org/api-guide/generic-views/#creating-custom-mixins
+class MultipleFieldLookupMixin:
+    """
+    Apply this mixin to any view or viewset to get multiple field filtering
+    based on a `lookup_fields` attribute, instead of the default single field filtering.
+    """
+    def get_object(self):
+        queryset = self.get_queryset()             # Get the base queryset
+        queryset = self.filter_queryset(queryset)  # Apply any filter backends
+        filter = {}
+        for field in self.lookup_fields:
+            if self.kwargs[field]: # Ignore empty fields.
+                filter[field] = self.kwargs[field]
+        obj = get_object_or_404(queryset, **filter)  # Lookup the object
+        self.check_object_permissions(self.request, obj)
+        return obj
 
 
 # Django REST Framework to allow the automatic downloading of data!
