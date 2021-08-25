2f1372771f6dfd98ab199c1eed2c1fc2741860d5
Mel <mkimble88@gmail.com>
2021-08-25 14:52:21 -0400

    added filterbackend to freezer api

--- freezer_inventory/views.py
+++ freezer_inventory/views.py
@@ -2,36 +2,41 @@ from django.shortcuts import render
 from .serializers import FreezerSerializer, FreezerRackSerializer, FreezerBoxSerializer, FreezerInventorySerializer, \
     FreezerCheckoutSerializer
 from .models import Freezer, FreezerRack, FreezerBox, FreezerInventory, FreezerCheckout
-from utility.serializers import MultipleFieldLookupMixin
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 # Create your views here.
 
 
-class FreezerViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
+class FreezerViewSet(viewsets.ModelViewSet):
     serializer_class = FreezerSerializer
     queryset = Freezer.objects.all()
-    lookup_fields = ['created_by']
+    filterset_fields = ['created_by']
+    filter_backends = [DjangoFilterBackend]
 
 
-class FreezerRackViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
+class FreezerRackViewSet(viewsets.ModelViewSet):
     serializer_class = FreezerRackSerializer
     queryset = FreezerRack.objects.all()
-    lookup_fields = ['freezer', 'created_by']
+    filterset_fields = ['freezer', 'created_by']
+    filter_backends = [DjangoFilterBackend]
 
 
-class FreezerBoxViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
+class FreezerBoxViewSet(viewsets.ModelViewSet):
     serializer_class = FreezerBoxSerializer
     queryset = FreezerBox.objects.all()
-    lookup_fields = ['freezer_rack', 'created_by']
+    filterset_fields = ['freezer_rack', 'created_by']
+    filter_backends = [DjangoFilterBackend]
 
 
-class FreezerInventoryViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
+class FreezerInventoryViewSet(viewsets.ModelViewSet):
     serializer_class = FreezerInventorySerializer
     queryset = FreezerInventory.objects.all()
-    lookup_fields = ['freezer_box', 'field_sample', 'extraction', 'created_by']
+    filterset_fields = ['freezer_box', 'field_sample', 'extraction', 'created_by']
+    filter_backends = [DjangoFilterBackend]
 
 
-class FreezerCheckoutViewSet(viewsets.ModelViewSet, MultipleFieldLookupMixin):
+class FreezerCheckoutViewSet(viewsets.ModelViewSet):
     serializer_class = FreezerCheckoutSerializer
     queryset = FreezerCheckout.objects.all()
-    lookup_fields = ['freezer_inventory', 'created_by']
+    filterset_fields = ['freezer_inventory', 'created_by']
+    filter_backends = [DjangoFilterBackend]
