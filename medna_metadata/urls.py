05835bc2df627a8e2e76ee9a186c3fd6f48bbb51
Mel <mkimble88@gmail.com>
2021-08-31 10:19:19 -0400

    removed extra url and re-registered all allauth urls

--- medna_metadata/urls.py
+++ medna_metadata/urls.py
@@ -168,28 +168,11 @@ urlpatterns = [
     # rest_auth urls
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    # allauth urls - added all to remove signup url
-    # url(r'^account/', include('allauth.urls')),
+    # allauth urls
+    url(r'^account/', include('allauth.urls')),
+    # re-registering signup to change url
     url(r'^account/disabled/signup/', signup, name='account_signup'),
-    url(r'^account/login/', login, name='account_login'),
-    url(r'^account/logout/', logout, name='account_logout'),
-    url(r'^account/password/change/', password_change, name='account_change_password',),
-    url(r'^account/password/set/', password_set, name='account_set_password'),
-    url(r'^account/inactive/', account_inactive, name='account_inactive'),
-    # allauth E-mail
-    url(r'^account/email/', email, name='account_email'),
-    url(r'^account/confirm-email/', email_verification_sent,
-        name='account_email_verification_sent',),
     # rest_auth and allauth email confirmation
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$',
         confirm_email, name='account_confirm_email'),
-    # allauth password reset
-    url(r'^account/password/reset/', password_reset, name='account_reset_password'),
-    url(r'^account/password/reset/done/', password_reset_done,
-        name='account_reset_password_done',),
-    url(r'^account/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$',
-        password_reset_from_key,
-        name='account_reset_password_from_key',),
-    url(r'^account/password/reset/key/done/', password_reset_from_key_done,
-        name='account_reset_password_from_key_done',),
 ]
