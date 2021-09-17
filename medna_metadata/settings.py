0d8ba3a1cf39b2c4673913848325ce689207c7a2
Mel <mkimble88@gmail.com>
2021-09-17 00:25:04 -0400

    changed region to us-east-1 in settings

--- medna_metadata/settings.py
+++ medna_metadata/settings.py
@@ -403,7 +403,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', 'https://s3.wasabisys.com')
-AWS_REGION = 'us-east-2'
+AWS_REGION = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', '%s.s3.%s.wasabisys.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION))
 
 AWS_S3_OBJECT_PARAMETERS = {
