515d8137fcaaa2dce024448ff99d2eaed140aa35
Mel <mkimble88@gmail.com>
2021-09-17 00:18:40 -0400

    updated s3 endpoint url to correct address

--- medna_metadata/settings.py
+++ medna_metadata/settings.py
@@ -402,8 +402,9 @@ SWAGGER_SETTINGS = {
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', 'https://s3.us-east-2.wasabisys.com')
-AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', '%s.s3.us-east-2.wasabisys.com' % AWS_STORAGE_BUCKET_NAME)
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', 'https://s3.wasabisys.com')
+AWS_REGION = 'us-east-2'
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN', '%s.s3.%s.wasabisys.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION))
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
--- medna_metadata/storage_backends.py
+++ medna_metadata/storage_backends.py
@@ -4,12 +4,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
+    # endpoint_url = settings.AWS_S3_ENDPOINT_URL
     file_overwrite = True
+    default_acl = 'public-read'
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
+    default_acl = 'public-read'
 
 
 class PrivateMediaStorage(S3Boto3Storage):
