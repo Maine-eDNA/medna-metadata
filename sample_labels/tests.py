8643b75e260804e813bd1955bf5e6e3667dcf2bd
Mel <mkimble88@gmail.com>
2021-10-17 12:54:14 -0400

    readded testcase for sampletype and converted old sampletype to samplematerial in tests.py in sample_labels app

--- sample_labels/tests.py
+++ sample_labels/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import SampleType, SampleLabel, SampleLabelRequest
+from .models import SampleMaterial, SampleLabel, SampleLabelRequest, SampleType
 from field_sites.models import FieldSite
 from field_sites.tests import FieldSiteTestCase
 # Create your tests here.
@@ -8,13 +8,30 @@ from field_sites.tests import FieldSiteTestCase
 class SampleTypeTestCase(TestCase):
     # fixtures = ['sample_labels_sampletype.json']
     def setUp(self):
-        SampleType.objects.update_or_create(sample_type_label="Sediment", sample_type_code="s")
-        SampleType.objects.update_or_create(sample_type_label="Water", sample_type_code="w")
+        SampleType.objects.update_or_create(sample_type_label="Subcore", sample_type_code="s")
+        SampleType.objects.update_or_create(sample_type_label="Filter", sample_type_code="f")
+        SampleType.objects.update_or_create(sample_type_label="Pooledlibrary", sample_type_code="p")
 
     def test_was_added_recently(self):
         # test if date is added correctly
-        sediment = SampleType.objects.get(sample_type_code="s")
-        water = SampleType.objects.get(sample_type_code="w")
+        subcore = SampleType.objects.get(sample_material_code="s")
+        filter = SampleType.objects.get(sample_material_code="f")
+        pooled_lib = SampleType.objects.get(sample_material_code="p")
+        self.assertIs(subcore.was_added_recently(), True)
+        self.assertIs(filter.was_added_recently(), True)
+        self.assertIs(pooled_lib.was_added_recently(), True)
+
+
+class SampleMaterialTestCase(TestCase):
+    # fixtures = ['sample_labels_samplematerial.json']
+    def setUp(self):
+        SampleMaterial.objects.update_or_create(sample_material_label="Sediment", sample_material_code="s")
+        SampleMaterial.objects.update_or_create(sample_material_label="Water", sample_material_code="w")
+
+    def test_was_added_recently(self):
+        # test if date is added correctly
+        sediment = SampleMaterial.objects.get(sample_material_code="s")
+        water = SampleMaterial.objects.get(sample_material_code="w")
         self.assertIs(sediment.was_added_recently(), True)
         self.assertIs(water.was_added_recently(), True)
 
@@ -24,15 +41,15 @@ class SampleLabelRequestTestCase(TestCase):
     def setUp(self):
         # get first record in queryset
         field_site_test = FieldSiteTestCase()
-        sample_type_test = SampleTypeTestCase()
+        sample_material_test = SampleMaterialTestCase()
         field_site_test.setUp()
-        sample_type_test.setUp()
+        sample_material_test.setUp()
         site_id = FieldSite.objects.filter()[:1].get()
-        sample_type = SampleType.objects.filter()[:1].get()
+        sample_material = SampleMaterial.objects.filter()[:1].get()
         # insert into db
-        SampleLabelRequest.objects.update_or_create(site_id=site_id, sample_type=sample_type, sample_year=2021,
+        SampleLabelRequest.objects.update_or_create(site_id=site_id, sample_material=sample_material, sample_year=2021,
                                                     purpose="SampleLabelTest1", req_sample_label_num=30)
-        SampleLabelRequest.objects.update_or_create(site_id=site_id, sample_type=sample_type, sample_year=2021,
+        SampleLabelRequest.objects.update_or_create(site_id=site_id, sample_material=sample_material, sample_year=2021,
                                                     purpose="SampleLabelTest2", req_sample_label_num=30)
 
     def test_was_added_recently(self):
