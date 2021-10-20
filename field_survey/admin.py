c8bfba1f34f51119b4f116b00d9494663bf47f44
Mel <mkimble88@gmail.com>
2021-10-20 15:23:44 -0400

    changed django.contrib admin to django.contrib.gis admin in field_survey\admin.,py

--- field_survey/admin.py
+++ field_survey/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+# from django.contrib import admin
+from django.contrib.gis import admin
 # from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter
 from .resources import FieldSurveyAdminResource, \
     FieldCrewAdminResource, EnvMeasurementAdminResource, \
