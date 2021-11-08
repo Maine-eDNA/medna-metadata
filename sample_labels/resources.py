17d009a164915abe334aa5d1e5891560c0e2c3a4
Mel <mkimble88@gmail.com>
2021-11-08 18:43:36 -0500

    commented out fields and added user to be added in before_import_row to SampleLabelRequestAdminResource

--- sample_labels/resources.py
+++ sample_labels/resources.py
@@ -27,9 +27,9 @@ class SampleLabelRequestAdminResource(resources.ModelResource):
     class Meta:
         model = SampleLabelRequest
         import_id_fields = ('site_id', 'sample_material', 'purpose', 'sample_year', 'req_sample_label_num',)
-        fields = ('sample_label_prefix', 'req_sample_label_num', 'min_sample_label_num', 'max_sample_label_num',
-                  'min_sample_label_id', 'max_sample_label_id', 'site_id', 'sample_year', 'sample_material',
-                  'purpose', 'created_by', 'created_datetime',)
+        #fields = ('sample_label_prefix', 'req_sample_label_num', 'min_sample_label_num', 'max_sample_label_num',
+        #          'min_sample_label_id', 'max_sample_label_id', 'site_id', 'sample_year', 'sample_material',
+        #          'purpose', 'created_by', 'created_datetime',)
         export_order = ('sample_label_prefix', 'req_sample_label_num', 'min_sample_label_num',
                         'max_sample_label_num', 'min_sample_label_id', 'max_sample_label_id', 'site_id',
                         'sample_year', 'sample_material', 'purpose', 'created_by', 'created_datetime',)
