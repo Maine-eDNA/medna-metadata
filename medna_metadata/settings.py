7c457196489c672a1d452017629bca02e5faf8ca
melkimble <mkimble88@gmail.com>
2021-11-10 18:32:34 +0000

    added installed_apps for django_extensions for generating schemas in INSTALLED_APPS in settings.py

--- medna_metadata/settings.py
+++ medna_metadata/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken', # for the creation of api tokens
     'phonenumber_field', # specific formatting for phone numbers - django-phonenumber-field[phonenumberslite]
     'crispy_forms', # crispy forms for pretty forms
+    #'django_extensions', # generating schema pngs
 ]
 
 # Internationalization
