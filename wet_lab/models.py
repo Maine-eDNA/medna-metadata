38e7c75cdd9c4a03847ae212eea00ad114332269
Mel <mkimble88@gmail.com>
2021-11-15 17:16:25 -0500

    moved update_extraction_status to utility/updates.py

--- wet_lab/models.py
+++ wet_lab/models.py
@@ -9,28 +9,11 @@ from field_survey.models import FieldSample
 from utility.models import DateTimeUserMixin, ProcessLocation, slug_date_format
 from utility.enumerations import TargetGenes, ConcentrationUnits, PhiXConcentrationUnits, VolUnits, LibPrepTypes, \
     DdpcrUnits, QpcrUnits, YesNo, LibPrepKits
+from utility.updates import update_extraction_status
 from medna_metadata.settings import DEFAULT_PROCESS_LOCATION_ID
 from django.utils import timezone
 # custom private media S3 backend storage
 from medna_metadata.storage_backends import PrivateSequencingStorage
-
-
-def update_extraction_status(old_barcode, new_barcode_pk):
-    # update is_extracted status of FieldSample model when samples are added to
-    # Extraction model
-    if old_barcode is not None:
-        # if it is not a new barcode, update the new to is_extracted status to YES
-        # and old to is_extracted status to NO
-        sample_obj = FieldSample.objects.filter(pk=new_barcode_pk).first()
-        new_barcode = sample_obj.barcode_slug
-        if old_barcode != new_barcode:
-            # compare old barcode to new barcode; if they are equal then we do not need
-            # to update
-            FieldSample.objects.filter(barcode_slug=old_barcode).update(is_extracted=YesNo.NO)
-            FieldSample.objects.filter(pk=new_barcode_pk).update(is_extracted=YesNo.YES)
-    else:
-        # if it is a new barcode, update the is_extracted status to YES
-        FieldSample.objects.filter(pk=new_barcode_pk).update(is_extracted=YesNo.YES)
 
 
 # Create your models here.
