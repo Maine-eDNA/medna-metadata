8f9109288827fabf0ada246d0a40d5ea8107eb49
Mel <mkimble88@gmail.com>
2021-11-15 17:46:07 -0500

    moved insert_update_sample_id_req to sample_labels models from utilities updates

--- utility/updates.py
+++ utility/updates.py
@@ -1,52 +1,11 @@
 from field_survey.models import FieldSample
-from sample_labels.models import SampleLabel
+#from sample_labels.models import SampleLabel
 from utility.enumerations import YesNo
-import numpy as np
+#import numpy as np
 # definitions that update model fields based on db insert/updates
 
 
 # SAMPLE_LABELS
-def insert_update_sample_id_req(sample_label_request, min_sample_label_id, max_sample_label_id, min_sample_label_num,
-                                max_sample_label_num, sample_label_prefix, site_id, sample_material, sample_type,
-                                sample_year, purpose):
-    if min_sample_label_id == max_sample_label_id:
-        # only one label request, so min and max label id will be the same; only need to enter
-        # one new label into SampleLabel
-        sample_label_id = min_sample_label_id
-        SampleLabel.objects.update_or_create(
-            sample_label_id=sample_label_id,
-            defaults={
-                'sample_label_request': sample_label_request,
-                'site_id': site_id,
-                'sample_material': sample_material,
-                'sample_type': sample_type,
-                'sample_year': sample_year,
-                'purpose': purpose,
-            }
-        )
-    else:
-        # more than one label requested, so need to interate to insert into SampleLabel
-        # arrange does not include max value, hence max+1
-        for num in np.arange(min_sample_label_num, max_sample_label_num + 1, 1):
-            # add leading zeros to site_num, e.g., 1 to 01
-            num_leading_zeros = str(num).zfill(4)
-
-            # format site_id, e.g., "eAL_L01"
-            sample_label_id = '{labelprefix}_{sitenum}'.format(labelprefix=sample_label_prefix,
-                                                               sitenum=num_leading_zeros)
-            # enter each new label into SampleLabel - request only has a single row with the requested
-            # number and min/max; this table is necessary for joining proceeding tables
-            SampleLabel.objects.update_or_create(
-                sample_label_id=sample_label_id,
-                defaults={
-                    'sample_label_request': sample_label_request,
-                    'site_id': site_id,
-                    'sample_material': sample_material,
-                    'sample_type': sample_type,
-                    'sample_year': sample_year,
-                    'purpose': purpose,
-                }
-            )
 
 
 # WET_LAB
