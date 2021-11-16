3519a4d30d77f2ef86b94305c900bcba6c156d9d
Mel <mkimble88@gmail.com>
2021-11-15 22:26:46 -0500

    fixed style formatting

--- utility/models.py
+++ utility/models.py
@@ -39,9 +39,7 @@ def get_default_process_location():
 # Create your models here.
 class DateTimeUserMixin(models.Model):
     # these are django fields for when the record was created and by whom
-    created_by = models.ForeignKey(get_user_model(), null=True,
-                                   on_delete=models.SET(get_sentinel_user),
-                                   default=get_default_user)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user), default=get_default_user)
     modified_datetime = models.DateTimeField("Modified DateTime", auto_now_add=True)
     created_datetime = models.DateTimeField("Created DateTime", auto_now=True)
 
