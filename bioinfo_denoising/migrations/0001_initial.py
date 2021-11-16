791e7c167f73cffda315b5b31b9ac47ce0fdd94f
Mel <mkimble88@gmail.com>
2021-11-15 22:48:58 -0500

    moved addfields to createmodel and rearranged models in bioinfo_denoising migrations

--- bioinfo_denoising/migrations/0001_initial.py
+++ bioinfo_denoising/migrations/0001_initial.py
@@ -18,22 +18,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='AmpliconSequenceVariant',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('modified_datetime', models.DateTimeField(auto_now_add=True, verbose_name='Modified DateTime')),
-                ('created_datetime', models.DateTimeField(auto_now=True, verbose_name='Created DateTime')),
-                ('asv_id', models.CharField(max_length=255, verbose_name='ASV ID')),
-                ('asv_sequence', models.TextField(verbose_name='ASV Sequence')),
-                ('asv_slug', models.SlugField(max_length=255, verbose_name='ASV Slug')),
-                ('created_by', models.ForeignKey(default=utility.models.get_default_user, on_delete=models.SET(utility.models.get_sentinel_user), to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'verbose_name': 'Amplicon Sequence Variant (ASV)',
-                'verbose_name_plural': 'Amplicon Sequence Variants (ASVs)',
-            },
-        ),
-        migrations.CreateModel(
             name='DenoisingMethod',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -42,7 +26,9 @@ class Migration(migrations.Migration):
                 ('denoising_method_name', models.CharField(max_length=255, verbose_name='Denoising Method Name')),
                 ('denoising_method_pipeline', models.CharField(max_length=255, verbose_name='Denoising Pipeline')),
                 ('denoising_method_slug', models.SlugField(max_length=255, verbose_name='Denoising Slug')),
-                ('created_by', models.ForeignKey(default=utility.models.get_default_user, on_delete=models.SET(utility.models.get_sentinel_user), to=settings.AUTH_USER_MODEL)),
+                ('created_by', models.ForeignKey(default=utility.models.get_default_user,
+                                                 on_delete=models.SET(utility.models.get_sentinel_user),
+                                                 to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'verbose_name': 'Denoising Method',
@@ -61,15 +47,38 @@ class Migration(migrations.Migration):
                 ('analyst_first_name', models.CharField(max_length=255, verbose_name='Analyst First Name')),
                 ('analyst_last_name', models.CharField(max_length=255, verbose_name='Analyst Last Name')),
                 ('analysis_sop_url', models.URLField(max_length=255, verbose_name='Analysis SOP URL')),
-                ('analysis_script_repo_url', models.URLField(default='https://github.com/Maine-eDNA', max_length=255, verbose_name='Repository URL')),
-                ('created_by', models.ForeignKey(default=utility.models.get_default_user, on_delete=models.SET(utility.models.get_sentinel_user), to=settings.AUTH_USER_MODEL)),
-                ('denoising_method', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='bioinfo_denoising.denoisingmethod')),
-                ('process_location', models.ForeignKey(default=utility.models.get_default_process_location, on_delete=django.db.models.deletion.RESTRICT, to='utility.processlocation')),
+                ('analysis_script_repo_url', models.URLField(default='https://github.com/Maine-eDNA', max_length=255,
+                                                             verbose_name='Repository URL')),
+                ('created_by', models.ForeignKey(default=utility.models.get_default_user,
+                                                 on_delete=models.SET(utility.models.get_sentinel_user),
+                                                 to=settings.AUTH_USER_MODEL)),
+                ('denoising_method', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT,
+                                                       to='bioinfo_denoising.denoisingmethod')),
+                ('process_location', models.ForeignKey(default=utility.models.get_default_process_location,
+                                                       on_delete=django.db.models.deletion.RESTRICT,
+                                                       to='utility.processlocation')),
                 ('run_result', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='wet_lab.runresult')),
             ],
             options={
                 'verbose_name': 'Denoising Metadata',
                 'verbose_name_plural': 'Denoising Metadata',
+            },
+        ),
+        migrations.CreateModel(
+            name='AmpliconSequenceVariant',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('modified_datetime', models.DateTimeField(auto_now_add=True, verbose_name='Modified DateTime')),
+                ('created_datetime', models.DateTimeField(auto_now=True, verbose_name='Created DateTime')),
+                ('asv_id', models.CharField(max_length=255, verbose_name='ASV ID')),
+                ('asv_sequence', models.TextField(verbose_name='ASV Sequence')),
+                ('asv_slug', models.SlugField(max_length=255, verbose_name='ASV Slug')),
+                ('created_by', models.ForeignKey(default=utility.models.get_default_user, on_delete=models.SET(utility.models.get_sentinel_user), to=settings.AUTH_USER_MODEL)),
+                ('denoising_metadata', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='bioinfo_denoising.denoisingmetadata')),
+            ],
+            options={
+                'verbose_name': 'Amplicon Sequence Variant (ASV)',
+                'verbose_name_plural': 'Amplicon Sequence Variants (ASVs)',
             },
         ),
         migrations.CreateModel(
@@ -87,10 +96,5 @@ class Migration(migrations.Migration):
                 'verbose_name': 'ASV Read',
                 'verbose_name_plural': 'ASV Reads',
             },
-        ),
-        migrations.AddField(
-            model_name='ampliconsequencevariant',
-            name='denoising_metadata',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='bioinfo_denoising.denoisingmetadata'),
         ),
     ]
