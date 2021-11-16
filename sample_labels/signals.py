e67ee45254cf168338a849dbd0746247cad486ac
Mel <mkimble88@gmail.com>
2021-11-16 04:28:37 -0500

    changed celery decorator to shared_task

--- sample_labels/signals.py
+++ sample_labels/signals.py
@@ -1,6 +1,6 @@
 # https://stackoverflow.com/questions/2719038/where-should-signal-handlers-live-in-a-django-project
 # https://docs.djangoproject.com/en/3.2/topics/signals/
-from .tasks import sample_label_request_post_save_task
+from sample_labels.tasks import sample_label_request_post_save_task
 from sample_labels.models import SampleLabelRequest
 from django.db.models.signals import post_save
 from django.dispatch import receiver
