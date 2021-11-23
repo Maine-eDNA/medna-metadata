b4da00d6cb0a4e8a015849cacd185fe25ba92321
Mel <mkimble88@gmail.com>
2021-11-22 19:40:14 -0500

    updated files for deploying docker

---
+++ requirements/docker-requirements.sh
@@ -0,0 +1,14 @@
+# Ubuntu 20.04
+# sudo bash ubuntu-requirements.sh
+apt-get update && apt-get install -y --no-install-recommends apt-utils
+# VIM text editor
+apt update && apt -y install \
+ vim
+# GDAL, GEOS, PROJ.4, python deps
+apt-get -y install \
+ binutils libproj-dev gdal-bin \
+ python3-dev python3-pip python3-venv python3-wheel
+# GIT
+apt-get -y install \
+ git \
+ && rm -rf /var/lib/apt/lists/*
