feadd764b181f6daf05577ed5accf8ec5e512163
Mel <mkimble88@gmail.com>
2021-11-22 21:40:15 -0500

    updated s3 storage_backends to use django.files.storage.FileSystemStorage if in testing

--- medna_metadata/storage_backends.py
+++ medna_metadata/storage_backends.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.core.files.storage import get_storage_class
 
 
 class StaticStorage(S3Boto3Storage):
@@ -24,3 +25,19 @@ class PrivateSequencingStorage(S3Boto3Storage):
     location = settings.AWS_PRIVATE_SEQUENCING_LOCATION
     default_acl = 'private'
     file_overwrite = False
+
+
+# https://stackoverflow.com/questions/59437637/django-use-private-s3-storage-only-in-production-environment
+def select_private_media_storage():
+    private_storage_class = get_storage_class(settings.PRIVATE_FILE_STORAGE)
+    return private_storage_class()  # instantiate the storage
+
+
+def select_private_sequencing_storage():
+    private_storage_class = get_storage_class(settings.PRIVATE_SEQUENCING_FILE_STORAGE)
+    return private_storage_class()  # instantiate the storage
+
+
+def select_public_media_storage():
+    public_storage_class = get_storage_class(settings.DEFAULT_FILE_STORAGE)
+    return public_storage_class()
