579db553d9ca99247363117ccba25ac10cd09a6c
Mel <mkimble88@gmail.com>
2021-11-22 22:09:41 -0500

    added postgres to docker-requirements.sh

--- requirements/docker-requirements.sh
+++ requirements/docker-requirements.sh
@@ -3,11 +3,14 @@
 apt-get update && apt-get install -y --no-install-recommends apt-utils
 # VIM text editor
 apt update && apt -y install \
- vim
+ libpq-dev postgresql postgresql-contrib vim
 # GDAL, GEOS, PROJ.4, python deps
 apt-get -y install \
  binutils libproj-dev gdal-bin \
  python3-dev python3-pip python3-venv python3-wheel
+# PostGIS
+apt -y install \
+ postgis postgresql-12-postgis-3
 # GIT
 apt-get -y install \
  git
