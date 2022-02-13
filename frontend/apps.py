05e0aa03586875d0d098f6ea5f775b5e4ac2ea2f
Mel <mkimble88@gmail.com>
2022-02-13 17:41:54 -0500

    added django-material-kit apps to frontend

--- frontend/apps.py
+++ frontend/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class FrontendConfig(AppConfig):
+class AppsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'frontend'
+    label = 'frontend'
