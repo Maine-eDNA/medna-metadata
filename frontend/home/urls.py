1a250574b0bd6b830346d167d0ac105ea326be18
Mel <mkimble88@gmail.com>
2022-02-17 18:28:21 -0500

    readded regex for main pages in frontend

--- frontend/home/urls.py
+++ frontend/home/urls.py
@@ -7,12 +7,12 @@ from django.urls import path, re_path
 from frontend.home import views
 
 urlpatterns = [
-
+    # https://stackoverflow.com/questions/59907011/matching-either-pattern-with-re-path-in-django-3-0
     # The home page
     path('', views.main_index, name='home'),
     path('dashboard/', views.dashboard_index, name='dashboard'),
     # Matches any html file
-    re_path(r'^.*\.*', views.main_pages, name='main_pages'),
+    re_path(r'^[main]+/.*\.*', views.main_pages, name='main_pages'),
     re_path(r'^[dashboard]+/.*\.*', views.dashboard_pages, name='dashboard_pages'),
 
 
