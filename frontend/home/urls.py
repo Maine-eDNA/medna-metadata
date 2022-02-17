d3fae3638c177dfea987607e2517f567c6ccd31e
Mel <mkimble88@gmail.com>
2022-02-17 18:22:53 -0500

    changed regex pattern for main pages

--- frontend/home/urls.py
+++ frontend/home/urls.py
@@ -9,10 +9,10 @@ from frontend.home import views
 urlpatterns = [
 
     # The home page
-    path('main/', views.main_index, name='home'),
+    path('', views.main_index, name='home'),
     path('dashboard/', views.dashboard_index, name='dashboard'),
     # Matches any html file
-    re_path(r'^[main]+/.*\.*', views.main_pages, name='main_pages'),
+    re_path(r'^.*\.*', views.main_pages, name='main_pages'),
     re_path(r'^[dashboard]+/.*\.*', views.dashboard_pages, name='dashboard_pages'),
 
 
