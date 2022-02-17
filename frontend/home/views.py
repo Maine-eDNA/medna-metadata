34d102c0c928161006b9396133512521e32291f1
Mel <mkimble88@gmail.com>
2022-02-16 21:44:47 -0500

    added remaining django-material-kit templates and css

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -14,7 +14,7 @@ from django.urls import reverse
 def index(request):
     context = {'segment': 'index'}
 
-    html_template = loader.get_template('home/index.html')
+    html_template = loader.get_template('home/django-datta-able/index.html')
     return HttpResponse(html_template.render(context, request))
 
 
@@ -31,14 +31,14 @@ def pages(request):
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
 
-        html_template = loader.get_template('home/' + load_template)
+        html_template = loader.get_template('home/django-datta-able/' + load_template)
         return HttpResponse(html_template.render(context, request))
 
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template('home/page-404.html')
+        html_template = loader.get_template('home/django-datta-able/page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
-        html_template = loader.get_template('home/page-500.html')
+        html_template = loader.get_template('home/django-datta-able/page-500.html')
         return HttpResponse(html_template.render(context, request))
