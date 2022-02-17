c3004b60d552f898b67236e3927a203b70b2eaba
Mel <mkimble88@gmail.com>
2022-02-17 17:59:51 -0500

    updated template urls for dashboard in frontend

--- frontend/home/urls.py
+++ frontend/home/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', views.dashboard_index, name='dashboard'),
     # Matches any html file
     re_path(r'^.*\.*', views.main_pages, name='main_pages'),
-    re_path(r'^.*\.*', views.dashboard_pages, name='dashboard_pages'),
+    re_path(r'^(?<=dashboard/).*\.*', views.dashboard_pages, name='dashboard_pages'),
 
 
 
