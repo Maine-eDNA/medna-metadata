216b5003e0ff4145f2cebdc2547f7c1a249e6763
Mel <mkimble88@gmail.com>
2022-02-23 16:31:07 -0500

    removed on_delete from m2m fields in utility models

--- utility/models.py
+++ utility/models.py
@@ -69,8 +69,7 @@ class Grant(DateTimeUserMixin):
     grant_description = models.TextField("Grant Description", blank=True)
 
     def __str__(self):
-        return '{code}: {label}'.format(code=self.grant_code,
-                                        label=self.grant_label)
+        return '{code}: {label}'.format(code=self.grant_code, label=self.grant_label)
 
     class Meta:
         app_label = 'utility'
@@ -96,7 +95,7 @@ class Project(DateTimeUserMixin):
     project_label = models.CharField("Project Label", max_length=255)
     project_description = models.TextField("Project Description", blank=True)
     project_research_questions = models.TextField("Project Research Questions", blank=True)
-    grant_names = models.ManyToManyField(Grant, on_delete=models.RESTRICT, verbose_name="Affiliated Grant(s)", related_name="grant_names")
+    grant_names = models.ManyToManyField(Grant, verbose_name="Affiliated Grant(s)", related_name="grant_names")
 
     def __str__(self):
         return '{label}'.format(label=self.project_label)
@@ -110,8 +109,8 @@ class Project(DateTimeUserMixin):
 class Publication(DateTimeUserMixin):
     publication_title = models.CharField("Publication Title", unique=True, max_length=255)
     publication_url = models.URLField("Publication URL", max_length=255)
-    project_names = models.ManyToManyField(Project, on_delete=models.RESTRICT, verbose_name="Affiliated Project(s)", related_name="project_names")
-    publication_authors = models.ManyToManyField(get_user_model(), on_delete=models.RESTRICT, verbose_name="Affiliated Authors(s)", related_name="publication_authors")
+    project_names = models.ManyToManyField(Project, verbose_name="Affiliated Project(s)", related_name="project_names")
+    publication_authors = models.ManyToManyField(get_user_model(), verbose_name="Affiliated Authors(s)", related_name="publication_authors")
     publication_slug = models.SlugField("Publication Slug", max_length=255)
 
     def save(self, *args, **kwargs):
