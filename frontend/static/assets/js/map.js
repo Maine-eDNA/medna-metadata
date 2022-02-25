2f0c7a128bc8185eea3ab273dbd5b2066904e3c7
Mel <mkimble88@gmail.com>
2022-02-24 22:24:33 -0500

    updated frontend templates

--- frontend/static/assets/js/map.js
+++ frontend/static/assets/js/map.js
@@ -1,6 +1,26 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const map = L.map('map')
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
+// create the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: attribution
+}).addTo(map);
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+// grab markers context from ProjectSurveyListView
 const markers = JSON.parse(document.getElementById('markers-data').textContent);
-let feature = L.geoJSON(markers).bindPopup(function (layer) { return layer.feature.properties.site_name; }).addTo(map);
-map.fitBounds(feature.getBounds(), { padding: [100, 100] });
+
+let feature = L.geoJSON(markers, {
+    pointToLayer: function(feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).bindPopup(function (layer) { return layer.feature.properties.site_name; }).addTo(map);
+
+map.fitBounds(feature.getBounds(), { padding: [1000, 1000] });
