a48bbbe957464871be8c96f807235bd83701c776
Mel <mkimble88@gmail.com>
2022-02-24 23:35:11 -0500

    readded prefetch_related to ProjectSurveyListView

--- utility/views.py
+++ utility/views.py
@@ -35,7 +35,7 @@ class ProjectSurveyListView(LoginRequiredMixin, TemplateView):
         """Return the view context data."""
         context = super().get_context_data(**kwargs)
         self.project = get_object_or_404(Project, pk=self.kwargs['pk'])
-        context["markers"] = json.loads(serialize("geojson", FieldSurvey.objects.filter(project_ids=self.project).only('geom', 'survey_datetime', 'site_name')))
+        context["markers"] = json.loads(serialize("geojson", FieldSurvey.objects.prefetch_related('project_ids').filter(project_ids=self.project).only('geom', 'survey_datetime', 'site_name')))
         context["project"] = self.project
         return context
 
