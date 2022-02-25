bfb241761b701a3b94e0ff915f60a608ad794076
Mel <mkimble88@gmail.com>
2022-02-24 23:20:49 -0500

    updated map js in static for frontend

--- frontend/static/assets/js/map.js
+++ frontend/static/assets/js/map.js
@@ -8,26 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markersGroup = new L.MarkerClusterGroup();
 
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
 // grab markers context from ProjectSurveyListView
 const markers = JSON.parse(document.getElementById('markers-data').textContent);
 
-let feature = L.geoJSON(markers, {
-    pointToLayer: function(feature, latlng) {
-    return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-}).bindPopup(function (layer) { return layer.feature.properties.site_name; });
+let feature = L.geoJSON(markers}).bindPopup(function (layer) { return layer.feature.properties.site_name; });
 
 markersGroup.addLayer(feature);
 
 map.addLayer(markersGroup);
 
-map.fitBounds(feature.getBounds(), { padding: [1000, 1000] });
+map.fitBounds(feature.getBounds(), { padding: [100, 100] });
