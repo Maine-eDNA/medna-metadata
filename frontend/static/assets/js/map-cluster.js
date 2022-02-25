b96b7f6fee63c2fd5d03e85e65e0f0abc5ed329f
Mel <mkimble88@gmail.com>
2022-02-25 15:05:25 -0500

    updated frontend templates and js

--- frontend/static/assets/js/map-cluster.js
+++ frontend/static/assets/js/map-cluster.js
@@ -2,17 +2,21 @@
 // https://stackoverflow.com/questions/29824478/leaflet-markercluster-with-geojson
 // https://github.com/Leaflet/Leaflet.markercluster
 // https://leafletjs.com/examples/geojson/
+var markersGroup = new L.markerClusterGroup();
+
+// grab markers context from ProjectSurveyTemplateView
+var markers = JSON.parse(document.getElementById('markers-data').textContent);
+
+if (markers.length == 0) {
+    document.getElementById('markers-data').textContent="";
+} else {
+
 var attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 var map = L.map('map')
 // create the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: attribution
 }).addTo(map);
-
-var markersGroup = new L.markerClusterGroup();
-
-// grab markers context from ProjectSurveyTemplateView
-var markers = JSON.parse(document.getElementById('markers-data').textContent);
 
 var geoJsonLayer = L.geoJSON(markers, {
     onEachFeature: function (feature, layer) {
@@ -25,3 +29,5 @@ markersGroup.addLayer(geoJsonLayer);
 map.addLayer(markersGroup);
 
 map.fitBounds(geoJsonLayer.getBounds(), { padding: [100, 100] });
+
+};
