d219728eeb939d10edc1da1fab9d18b74972d76e
Mel <mkimble88@gmail.com>
2022-03-01 11:39:43 -0500

    updated contact us form

--- utility/forms.py
+++ utility/forms.py
@@ -3,7 +3,7 @@ from .models import ContactUs
 
 
 # FRONTEND
-class ContactUsForm(forms.Form):
+class ContactUsForm(forms.ModelForm):
     # https://simpleisbetterthancomplex.com/tutorial/2018/11/28/advanced-form-rendering-with-django-crispy-forms.html
     # https://simpleisbetterthancomplex.com/article/2017/08/19/how-to-render-django-form-manually.html
     full_name = forms.CharField(
@@ -36,6 +36,10 @@ class ContactUsForm(forms.Form):
             }
         )
     )
+
+    class Meta:
+        model = ContactUs
+        fields = ['full_name', 'contact_email', 'contact_context', ]
 
     # def send_email(self):
     #     # send email using the self.cleaned_data dictionary
