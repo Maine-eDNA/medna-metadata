b5169d793a05c59030280c191ee7950ea9f4ded9
Mel <mkimble88@gmail.com>
2022-03-01 10:31:35 -0500

    updated contact us form in frontend

--- utility/forms.py
+++ utility/forms.py
@@ -3,11 +3,39 @@ from .models import ContactUs
 
 
 # FRONTEND
-class ContactUsForm(forms.ModelForm):
+class ContactUsForm(forms.Form):
     # https://simpleisbetterthancomplex.com/tutorial/2018/11/28/advanced-form-rendering-with-django-crispy-forms.html
-    class Meta:
-        model = ContactUs
-        fields = ('full_name', 'contact_email', 'contact_context', )
+    # https://simpleisbetterthancomplex.com/article/2017/08/19/how-to-render-django-form-manually.html
+    full_name = forms.CharField(
+        required=True,
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={
+                'style': 'form-control',
+                'placeholder': 'Full Name'
+            }
+        )
+    )
+    contact_email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'style': 'form-control',
+                'placeholder': 'Full Name'
+            }
+        )
+    )
+    contact_context = forms.CharField(
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                'style': 'form-control',
+                'placeholder': 'Describe your problem in at least 250 characters',
+                'id': 'message',
+                'rows': 6,
+            }
+        )
+    )
 
     def send_email(self):
         # send email using the self.cleaned_data dictionary
