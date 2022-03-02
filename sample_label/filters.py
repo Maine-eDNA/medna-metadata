244981893c8e9258409ae0873c1647d21f5e16d5
Mel <mkimble88@gmail.com>
2022-03-01 23:25:01 -0500

    removed get_year_choices because throwing error on db creation

--- sample_label/filters.py
+++ sample_label/filters.py
@@ -6,7 +6,7 @@ from field_site.models import FieldSite
 
 class SampleLabelRequestFilter(filters.FilterSet):
     created_datetime = filters.DateFilter(input_formats=['%Y-%m-%d', '%d-%m-%Y'], lookup_expr='icontains', widget=forms.SelectDateWidget())
-    sample_year = filters.ChoiceFilter(choices=year_choices())
+    sample_year = filters.ChoiceFilter(choices=year_choices)
     sample_material = filters.ModelMultipleChoiceFilter(queryset=SampleMaterial.objects.all(), widget=forms.CheckboxSelectMultiple)
     sample_type = filters.ModelMultipleChoiceFilter(queryset=SampleType.objects.all(), widget=forms.CheckboxSelectMultiple)
     site_id = filters.ModelMultipleChoiceFilter(queryset=FieldSite.objects.all(), widget=forms.CheckboxSelectMultiple)
@@ -18,7 +18,7 @@ class SampleLabelRequestFilter(filters.FilterSet):
 
 class UserSampleLabelRequestFilter(filters.FilterSet):
     created_datetime = filters.DateFilter(input_formats=['%Y-%m-%d', '%d-%m-%Y'], lookup_expr='icontains', widget=forms.SelectDateWidget())
-    sample_year = filters.ChoiceFilter(choices=get_choices(SampleLabelRequest, 'sample_year'))
+    sample_year = filters.ChoiceFilter(choices=year_choices)
     sample_type = filters.ModelMultipleChoiceFilter(queryset=SampleType.objects.all(), widget=forms.CheckboxSelectMultiple)
     sample_material = filters.ModelMultipleChoiceFilter(queryset=SampleMaterial.objects.all(), widget=forms.CheckboxSelectMultiple)
     site_id = filters.ModelMultipleChoiceFilter(queryset=FieldSite.objects.all(), widget=forms.CheckboxSelectMultiple)
