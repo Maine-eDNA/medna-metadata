ab71431280382053cab1ca9524101ff6a03d7da4
Mel <mkimble88@gmail.com>
2022-03-03 16:18:52 -0500

    updated base table render template

--- sample_label/tables.py
+++ sample_label/tables.py
@@ -9,10 +9,10 @@ class SampleLabelRequestTable(tables.Table):
     # same as <a href="{% url 'users:samplelabel_detail' samplelabel.id %}"> {{ samplelabel.max_sample_label_id }}</a>
     max_sample_label_id = tables.LinkColumn(viewname='detail_samplelabelrequest',
                                             args=[A('pk')],
-                                            attrs={"th": {"class": "text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 field-max_sample_label_id"}})
+                                            attrs={"th": {"class": "field-max_sample_label_id"}})
     # Change column header
     min_sample_label_num = tables.Column(verbose_name="Min Label Num",
-                                         attrs={"th": {"class": "text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 field-min_sample_label_num"}})
+                                         attrs={"th": {"class": "field-min_sample_label_num"}})
     # Same as <a href="{% url 'users:samplelabel_samplelabel_add' samplelabel.site_id.id
     # samplelabel.sample_material.id samplelabel.purpose %}" class="addlink"> {% translate 'Add' %}</a>
     add_label = tables.LinkColumn(viewname="add_samplelabelrequestdetail",
@@ -21,7 +21,7 @@ class SampleLabelRequestTable(tables.Table):
                                   args=[A("site_id.id"), A("sample_material.id"), A("purpose")])
     # formatting for date column
     created_datetime = tables.DateTimeColumn(format="M d, Y",
-                                             attrs={"th": {"class": "text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 field-created_datetime"}})
+                                             attrs={"th": {"class": "field-created_datetime"}})
     _selected_action = tables.CheckBoxColumn(accessor="pk",
                                              attrs={"td": {"class": "action-checkbox"},
                                                     "input": {"class": "action-select"},
@@ -36,7 +36,7 @@ class SampleLabelRequestTable(tables.Table):
         model = SampleLabelRequest
         fields = ("_selected_action", "max_sample_label_id", "min_sample_label_num", "sample_year",
                   "sample_material", "purpose", "created_datetime")
-        attrs = {"class": "table align-items-center mb-0"}
+        # attrs = {"class": "table align-items-center mb-0"}
         # set table css class to "result_lust"
         # attrs = {"class": "result_list"}
         # this is NOT the template it writes to, this is the template it uses to load with
