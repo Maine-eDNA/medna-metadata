7c4940822c1968e74cfa5e01e95799125e113d96
Mel <mkimble88@gmail.com>
2022-03-05 18:15:55 -0500

    added extractions chart and totals to dashbboard

---
+++ frontend/static/assets/js/charts-wetlab.js
@@ -0,0 +1,97 @@
+$(function () {
+    // https://simpleisbetterthancomplex.com/tutorial/2020/01/19/how-to-use-chart-js-with-django.html
+
+  // EXTRACTION CHARTS
+  // line chart
+  var $chartExtractionCount = $("#chartExtractionCount");
+  var $extractionTotal = $("#extractionTotal");
+  $.ajax({
+    url: $chartExtractionCount.data("url"),
+    success: function (data) {
+
+    var extractionSum = data.data.reduce((partialSum, a) => partialSum + a, 0);
+
+    //console.log(surveySum);
+    $extractionTotal.text(extractionSum);
+
+    var ctx1 = $chartExtractionCount[0].getContext("2d");
+
+      new Chart(ctx1, {
+        type: "line",
+        data: {
+          labels: data.labels,
+          datasets: [{
+              label: "Extraction Count",
+              tension: 0.4,
+              borderWidth: 0,
+              pointRadius: 2,
+              pointBackgroundColor: "#e3316e",
+              borderColor: "#e3316e",
+              borderWidth: 3,
+              backgroundColor: 'transparent',
+              data: data.data,
+              maxBarThickness: 6
+            }],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              display: false,
+            }
+          },
+          interaction: {
+            intersect: false,
+            mode: 'index',
+          },
+          scales: {
+            y: {
+              grid: {
+                drawBorder: false,
+                display: true,
+                drawOnChartArea: true,
+                drawTicks: false,
+                borderDash: [5, 5]
+              },
+              ticks: {
+                display: true,
+                padding: 10,
+                color: '#b2b9bf',
+                font: {
+                  size: 11,
+                  family: "Open Sans",
+                  style: 'normal',
+                  lineHeight: 2
+                },
+              }
+            },
+            x: {
+              grid: {
+                drawBorder: false,
+                display: true,
+                drawOnChartArea: true,
+                drawTicks: true,
+                borderDash: [5, 5]
+              },
+              ticks: {
+                display: true,
+                color: '#b2b9bf',
+                padding: 10,
+                font: {
+                  size: 11,
+                  family: "Open Sans",
+                  style: 'normal',
+                  lineHeight: 2
+                },
+              }
+            },
+          },
+        },
+      });
+
+    }
+  });
+
+
+});
