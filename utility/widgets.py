a18c65a8b3840a0409cab0cf8c35e4dec4cbcb73
Mel <mkimble88@gmail.com>
2022-03-06 15:51:57 -0500

    Added custom templates for form inputs

--- utility/widgets.py
+++ utility/widgets.py
@@ -6,12 +6,36 @@ from django.template.loader import render_to_string
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'forms/widgets/clearable_file_input.html'
 
-    def render(self):
-        return mark_safe(render_to_string(self.template_name))
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        checkbox_name = self.clear_checkbox_name(name)
+        checkbox_id = self.clear_checkbox_id(checkbox_name)
+        context['widget'].update({
+            'checkbox_name': checkbox_name,
+            'checkbox_id': checkbox_id,
+            'is_initial': self.is_initial(value),
+            'input_text': self.input_text,
+            'initial_text': self.initial_text,
+            'clear_checkbox_label': self.clear_checkbox_label,
+            'template_name': self.template_name,
+        })
+        return context
 
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'forms/widgets/file.html'
 
-    def render(self):
-        return mark_safe(render_to_string(self.template_name))
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        checkbox_name = self.clear_checkbox_name(name)
+        checkbox_id = self.clear_checkbox_id(checkbox_name)
+        context['widget'].update({
+            'checkbox_name': checkbox_name,
+            'checkbox_id': checkbox_id,
+            'is_initial': self.is_initial(value),
+            'input_text': self.input_text,
+            'initial_text': self.initial_text,
+            'clear_checkbox_label': self.clear_checkbox_label,
+            'template_name': self.template_name,
+        })
+        return context
