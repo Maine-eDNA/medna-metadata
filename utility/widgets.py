11fc2450a65f3a6f81c497abc3fdae6d60d88fd0
Mel <mkimble88@gmail.com>
2022-03-06 16:31:16 -0500

    Changed template render menthod in utility widgets

--- utility/widgets.py
+++ utility/widgets.py
@@ -7,6 +7,18 @@ from django.template.loader import render_to_string
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'django/custom_forms/widgets/clearable_file_input.html'
 
+    def get_context(self, name, value, attrs):
+        return {
+            'widget': {
+                'name': name,
+                'is_hidden': self.is_hidden,
+                'required': self.is_required,
+                'value': self.format_value(value),
+                'attrs': self.build_attrs(self.attrs, attrs),
+                'template_name': self.template_name,
+            },
+        }
+
     def render(self, name, value, attrs=None):
         context = self.get_context(name, value, attrs)
         template = loader.get_template(self.template_name).render(context)
@@ -15,6 +27,18 @@ class CustomClearableFileInput(forms.ClearableFileInput):
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'django/custom_forms/widgets/file.html'
+
+    def get_context(self, name, value, attrs):
+        return {
+            'widget': {
+                'name': name,
+                'is_hidden': self.is_hidden,
+                'required': self.is_required,
+                'value': self.format_value(value),
+                'attrs': self.build_attrs(self.attrs, attrs),
+                'template_name': self.template_name,
+            },
+        }
 
     def render(self, name, value, attrs=None):
         context = self.get_context(name, value, attrs)
