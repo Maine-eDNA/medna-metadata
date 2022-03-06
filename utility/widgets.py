7be85454d51af7f1a07977620a58b92f357386eb
Mel <mkimble88@gmail.com>
2022-03-06 16:05:26 -0500

    updated utility widgets

--- utility/widgets.py
+++ utility/widgets.py
@@ -6,17 +6,9 @@ from django.template.loader import render_to_string
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'django/forms/widgets/clearable_file_input.html'
 
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        checkbox_name = self.clear_checkbox_name(name)
-        checkbox_id = self.clear_checkbox_id(checkbox_name)
+    def get_context(self, **kwargs):
+        context = super().get_context()
         context['widget'].update({
-            'checkbox_name': checkbox_name,
-            'checkbox_id': checkbox_id,
-            'is_initial': self.is_initial(value),
-            'input_text': self.input_text,
-            'initial_text': self.initial_text,
-            'clear_checkbox_label': self.clear_checkbox_label,
             'template_name': self.template_name,
         })
         return context
@@ -25,17 +17,9 @@ class CustomClearableFileInput(forms.ClearableFileInput):
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'django/forms/widgets/file.html'
 
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        checkbox_name = self.clear_checkbox_name(name)
-        checkbox_id = self.clear_checkbox_id(checkbox_name)
+    def get_context(self, **kwargs):
+        context = super().get_context()
         context['widget'].update({
-            'checkbox_name': checkbox_name,
-            'checkbox_id': checkbox_id,
-            'is_initial': self.is_initial(value),
-            'input_text': self.input_text,
-            'initial_text': self.initial_text,
-            'clear_checkbox_label': self.clear_checkbox_label,
             'template_name': self.template_name,
         })
         return context
