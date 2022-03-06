e487213c1cfa576f2758427d4387e354bfdd3c0d
Mel <mkimble88@gmail.com>
2022-03-06 16:29:52 -0500

    Changed template render menthod in utility widgets

--- utility/widgets.py
+++ utility/widgets.py
@@ -1,25 +1,22 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.template import loader
 from django.template.loader import render_to_string
 
 
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'django/custom_forms/widgets/clearable_file_input.html'
 
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context['widget'].update({
-            'template_name': self.template_name,
-        })
-        return context
+    def render(self, name, value, attrs=None):
+        context = self.get_context(name, value, attrs)
+        template = loader.get_template(self.template_name).render(context)
+        return mark_safe(template)
 
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'django/custom_forms/widgets/file.html'
 
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context['widget'].update({
-            'template_name': self.template_name,
-        })
-        return context
+    def render(self, name, value, attrs=None):
+        context = self.get_context(name, value, attrs)
+        template = loader.get_template(self.template_name).render(context)
+        return mark_safe(template)
