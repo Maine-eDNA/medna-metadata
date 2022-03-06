550d74a87ea616f0a07b162e017c992181cf3e53
Mel <mkimble88@gmail.com>
2022-03-06 15:42:56 -0500

    Added custom templates for form inputs

--- utility/widgets.py
+++ utility/widgets.py
@@ -1,9 +1,17 @@
 from django import forms
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 
 
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'forms/widgets/clearable_file_input.html'
 
+    def render(self):
+        return mark_safe(render_to_string(self.template_name))
+
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'forms/widgets/file.html'
+
+    def render(self):
+        return mark_safe(render_to_string(self.template_name))
