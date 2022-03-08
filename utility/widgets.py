9736e0cd0255991044bdec9083d386f7336766eb
Mel <mkimble88@gmail.com>
2022-03-08 16:16:31 -0500

    readded get_context to custom widgets

--- utility/widgets.py
+++ utility/widgets.py
@@ -7,6 +7,21 @@ from django.template import loader
 class CustomClearableFileInput(forms.ClearableFileInput):
     template_name = 'custom_forms/widgets/clearable_file_input.html'
 
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        checkbox_name = self.clear_checkbox_name(name)
+        checkbox_id = self.clear_checkbox_id(checkbox_name)
+        context['widget'].update({
+            'checkbox_name': checkbox_name,
+            'checkbox_id': checkbox_id,
+            'is_initial': self.is_initial(value),
+            'input_text': self.input_text,
+            'initial_text': self.initial_text,
+            'clear_checkbox_label': self.clear_checkbox_label,
+            'template_name': self.template_name,
+        })
+        return context
+
     def render(self, name, value, attrs=None, renderer=None):
         """Render the widget as an HTML string."""
         context = self.get_context(name, value, attrs)
@@ -15,6 +30,21 @@ class CustomClearableFileInput(forms.ClearableFileInput):
 
 class CustomFileInput(forms.ClearableFileInput):
     template_name = 'custom_forms/widgets/file.html'
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        checkbox_name = self.clear_checkbox_name(name)
+        checkbox_id = self.clear_checkbox_id(checkbox_name)
+        context['widget'].update({
+            'checkbox_name': checkbox_name,
+            'checkbox_id': checkbox_id,
+            'is_initial': self.is_initial(value),
+            'input_text': self.input_text,
+            'initial_text': self.initial_text,
+            'clear_checkbox_label': self.clear_checkbox_label,
+            'template_name': self.template_name,
+        })
+        return context
 
     def render(self, name, value, attrs=None, renderer=None):
         """Render the widget as an HTML string."""
