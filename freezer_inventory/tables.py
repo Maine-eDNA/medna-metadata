59732db873f321767e089eec704972c741bf5a8f
Mel <mkimble88@gmail.com>
2022-03-09 10:54:44 -0500

    updated freezer_inventory tbl to include accessor to select related field

--- freezer_inventory/tables.py
+++ freezer_inventory/tables.py
@@ -7,10 +7,12 @@ class FreezerInventoryReturnMetadataTable(tables.Table):
     freezer_log = tables.LinkColumn(verbose_name="Freezer Log",
                                     viewname='detail_freezerinventorylog',
                                     args=[A('freezer_log.id')],
-                                    attrs={"th": {"class": "field-freezer_log"}})
+                                    attrs={"th": {"class": "field-freezer_log"}},
+                                    accessor='freezer_log.freezer_log_slug')
     # Change column header
     freezer_return_metadata_entered = tables.Column(attrs={"th": {"class": "field-freezer_return_metadata_entered"}})
-    freezer_return_actions = tables.Column(attrs={"th": {"class": "field-freezer_return_actions"}})
+    freezer_return_actions = tables.Column(attrs={"th": {"class": "field-freezer_return_actions"}},
+                                           accessor='freezer_return_actions.action_label')
     freezer_return_vol_taken = tables.Column(attrs={"th": {"class": "field-freezer_return_vol_taken"}})
     freezer_return_vol_units = tables.Column(attrs={"th": {"class": "field-freezer_return_vol_units"}})
     freezer_return_notes = tables.Column(attrs={"th": {"class": "field-freezer_return_notes"}})
@@ -18,7 +20,7 @@ class FreezerInventoryReturnMetadataTable(tables.Table):
     created_datetime = tables.DateTimeColumn(format="M d, Y", attrs={"th": {"class": "field-created_datetime"}})
     edit = tables.LinkColumn(viewname='update_freezerinventoryreturnmetadata',
                              args=[A('pk')],
-                             # text='Edit',
+                             text='Edit',
                              attrs={"td": {"class": "fa fa-edit text-secondary font-weight-bold text-xs"}})
     _selected_action = tables.CheckBoxColumn(accessor="pk",
                                              attrs={"td": {"class": "action-checkbox"},
