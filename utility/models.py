43d4a1677c575598e8da3b234699b7826334b3a1
Mel <mkimble88@gmail.com>
2022-03-08 22:40:19 -0500

    added created_daypart for time of day returns

--- utility/models.py
+++ utility/models.py
@@ -172,6 +172,15 @@ class ContactUs(DateTimeUserMixin):
         created_month = self.created_datetime.month
         return season_dict.get(created_month)
 
+    @property
+    def created_daypart(self):
+        daypart_dict = {0: 1, 1: 1, 2: 1, 3: 1,  # Midnight - 3AM
+                        4: 2, 5: 2, 6: 2, 7: 2, 8: 2, 9: 2, 10: 2, 11: 2,  # 4AM - 11AM
+                        12: 3, 13: 3, 14: 3, 15: 3, 16: 3, 17: 3, 18: 3,  # Noon - 6PM
+                        19: 4, 20: 4, 21: 4, 22: 4, 23: 4}  # 7PM - 11PM
+        created_hour = self.created_datetime.hour
+        return daypart_dict.get(created_hour)
+
     def save(self, *args, **kwargs):
         if self.created_datetime is None:
             created_date_fmt = slug_date_format(timezone.now())
