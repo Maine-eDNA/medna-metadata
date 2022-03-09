b5471af05d6372e6c1af057746114f61b8533b56
Mel <mkimble88@gmail.com>
2022-03-09 16:53:39 -0500

    added custom select2 widgets

--- utility/widgets.py
+++ utility/widgets.py
@@ -15,3 +15,31 @@ class CustomFileInput(forms.FileInput):
 class CustomRadioSelect(forms.RadioSelect):
     template_name = 'custom_forms/widgets/radio.html'
     option_template_name = 'custom_forms/widgets/radio_option.html'
+
+
+class CustomSelect2(forms.Select):
+    class Media:
+        css = {'all': ('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css', )}
+        js = ('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', 'assets/js/custom-select2.js', )
+
+    template_name = 'custom_forms/widgets/select2.html'
+    option_template_name = 'custom_forms/widgets/select2_option.html'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['attrs'] = {'class': 'js-example-basic-single'}
+        super(CustomSelect2, self).__init__(*args, **kwargs)
+
+
+class CustomSelect2Multiple(forms.SelectMultiple):
+    class Media:
+        css = {'all': ('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css', )}
+        js = ('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', 'assets/js/custom-select2.js', )
+
+    template_name = 'custom_forms/widgets/select2.html'
+    option_template_name = 'custom_forms/widgets/select2_option.html'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['attrs'] = {'class': 'js-example-basic-multiple'}
+        super(CustomSelect2Multiple, self).__init__(*args, **kwargs)
+
+
