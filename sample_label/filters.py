74b3259198ab2dd37af0ef7dad415c60859912bf
Mel <mkimble88@gmail.com>
2022-03-09 21:30:08 -0500

    added css to some filter forms

--- sample_label/filters.py
+++ sample_label/filters.py
@@ -10,11 +10,15 @@ from utility.widgets import CustomSelect2Multiple
 # FRONTEND FILTERS                     #
 ########################################
 class SampleLabelRequestFilter(filters.FilterSet):
-    created_datetime = filters.DateFilter(input_formats=['%Y-%m-%d', '%d-%m-%Y'], lookup_expr='icontains', widget=forms.SelectDateWidget())
+    created_datetime = filters.DateFilter(input_formats=['%Y-%m-%d', '%d-%m-%Y'], lookup_expr='icontains',
+                                          widget=forms.SelectDateWidget(attrs={'class': 'form-control', }))
     sample_year = filters.ChoiceFilter(choices=year_choices)
-    sample_material = filters.ModelMultipleChoiceFilter(queryset=SampleMaterial.objects.all(), widget=CustomSelect2Multiple)
-    sample_type = filters.ModelMultipleChoiceFilter(queryset=SampleType.objects.all(), widget=CustomSelect2Multiple)
-    site_id = filters.ModelMultipleChoiceFilter(queryset=FieldSite.objects.all(), widget=CustomSelect2Multiple)
+    sample_material = filters.ModelMultipleChoiceFilter(queryset=SampleMaterial.objects.all(),
+                                                        widget=CustomSelect2Multiple)
+    sample_type = filters.ModelMultipleChoiceFilter(queryset=SampleType.objects.all(),
+                                                    widget=CustomSelect2Multiple)
+    site_id = filters.ModelMultipleChoiceFilter(queryset=FieldSite.objects.all(),
+                                                widget=CustomSelect2Multiple)
 
     class Meta:
         model = SampleLabelRequest
