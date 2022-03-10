449d04a50748c6895c9f65eccb548dc6b8e237f4
Mel <mkimble88@gmail.com>
2022-03-09 21:33:01 -0500

    fixed form.media reference in templates

--- sample_label/filters.py
+++ sample_label/filters.py
@@ -12,7 +12,8 @@ from utility.widgets import CustomSelect2Multiple
 class SampleLabelRequestFilter(filters.FilterSet):
     created_datetime = filters.DateFilter(input_formats=['%Y-%m-%d', '%d-%m-%Y'], lookup_expr='icontains',
                                           widget=forms.SelectDateWidget(attrs={'class': 'form-control', }))
-    sample_year = filters.ChoiceFilter(choices=year_choices)
+    sample_year = filters.MultipleChoiceFilter(choices=year_choices,
+                                               widget=CustomSelect2Multiple)
     sample_material = filters.ModelMultipleChoiceFilter(queryset=SampleMaterial.objects.all(),
                                                         widget=CustomSelect2Multiple)
     sample_type = filters.ModelMultipleChoiceFilter(queryset=SampleType.objects.all(),
