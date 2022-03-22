2a99d2f043198b0e47a55e1ce4fc2630c2053d0d
Mel <mkimble88@gmail.com>
2022-03-22 16:42:11 -0400

    Added humanize to settings and load in table.html for frontend templates

--- medna_metadata/settings.py
+++ medna_metadata/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.gis',
     'django.forms',
