f2eea95a6cae595e569fc90cb53a53310c7ff036
Mel <mkimble88@gmail.com>
2022-03-26 18:26:05 -0400

    updated float to include negative and positive values

--- utility/converters.py
+++ utility/converters.py
@@ -1,7 +1,7 @@
 # https://docs.djangoproject.com/en/4.0/topics/http/urls/#registering-custom-path-converters
 # URL filter tags eg <float>
 class FloatUrlParameterConverter:
-    regex = '[0-9]+\.?[0-9]+'
+    regex = '/^[-+]?[0-9]*\.?[0-9]+$/'
 
     def to_python(self, value):
         return float(value)
