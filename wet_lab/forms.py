ee490a0856e4d2deebfd14c56392d47ac664d241
Mel <mkimble88@gmail.com>
2022-03-30 22:41:57 -0400

    passed currently logged in user to kwargs in PcrForm

--- wet_lab/forms.py
+++ wet_lab/forms.py
@@ -302,9 +302,9 @@ class PcrForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         # https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-implement-dependent-or-chained-dropdown-list-with-django.html
-        super().__init__(*args, **kwargs)
         user = kwargs.pop('user')
         self.fields['pcr_replicate'].queryset = PcrReplicate.objects.filter(created_by=user).order_by('-created_datetime')
+        super(PcrForm, self).__init__(*args, **kwargs)
 
 
 class LibraryPrepForm(forms.ModelForm):
