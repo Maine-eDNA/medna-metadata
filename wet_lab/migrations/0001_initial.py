277a2e70923f363945ddeccb06dd01483c7298c5
Mel <mkimble88@gmail.com>
2022-04-14 22:26:12 -0400

    added null=true to primer_set in fastqfile

--- wet_lab/migrations/0001_initial.py
+++ wet_lab/migrations/0001_initial.py
@@ -333,7 +333,7 @@ class Migration(migrations.Migration):
                 ('uuid', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('run_result', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='wet_lab.runresult', related_name='run_results')),
                 ('extraction', models.ForeignKey(null=True, on_delete=django.db.models.deletion.RESTRICT, to='wet_lab.extraction', related_name='extractions')),
-                ('primer_set', models.ForeignKey(on_delete=django.db.models.deletion.RESTRICT, to='wet_lab.primerpair', related_name='primer_sets')),
+                ('primer_set', models.ForeignKey(null=True, on_delete=django.db.models.deletion.RESTRICT, to='wet_lab.primerpair', related_name='primer_sets')),
                 ('fastq_slug', models.SlugField(max_length=255, verbose_name='Fastq Slug')),
                 ('fastq_datafile', models.FileField(max_length=255, storage=medna_metadata.storage_backends.select_private_sequencing_storage, upload_to='', verbose_name='FastQ Datafile')),
                 ('submitted_to_insdc', models.CharField(choices=[(None, '(Unknown)'), ('no', 'No'), ('yes', 'Yes')], default='no', max_length=3, verbose_name='Submitted to INSDC')),
