756f2bb0ae3c3649208ad4d3fa58857a7705b897
Mel <mkimble88@gmail.com>
2022-06-20 15:43:20 -0400

    refactored datefmt into function returnDateFmt in local-contexts-api.js

--- frontend/static/assets/js/local-contexts-api.js
+++ frontend/static/assets/js/local-contexts-api.js
@@ -3,6 +3,12 @@ $(function () {
     var $lcProjectIdSpan = $("#lcProjectId")
     var $projectUniqueId = $lcProjectIdSpan.text();
     var $lcResponse = $("#lcResponse")
+
+    function returnDateFmt (date) {
+        var newDate = new Date(date)
+        var dateFmt = newDate.toLocaleString()
+        return dateFmt
+    }
     $.ajax({
         //type
         url: $localContextsUrl + $projectUniqueId,
@@ -15,10 +21,8 @@ $(function () {
                 $lcProjectIdSpan.remove()
                 $.each(data.notice, function(key, value){ // The contents inside notice
                     //console.log(value)
-                    var $updatedDate = new Date(value.updated)
-                    var $updatedDateFmt = $createdDate.toLocaleString
-                    var $createdDate = new Date(value.created)
-                    var $createdDateFmt = $createdDate.toLocaleString
+                    var $createdDateFmt = returnDateFmt(value.created);
+                    var $updatedDateFmt = returnDateFmt(value.updated);
                     if (value.notice_type == "biocultural") {
                         $lcResponse.html("<div class='row'><div class='col-md-3 col-12 my-auto'><a target='_blank' href="+value.bc_svg_url+"><img class='w-100 border-radius-lg shadow-lg' src="+value.bc_svg_url+" alt="+value.notice_type+"></a></div><div class='col-md-9 col-12 my-auto'><p class='pe-md-5'>"+value.bc_default_text+"</p><table class='table align-items-center mb-0' style='text-transform:capitalize;'><tbody><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Notice Type"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.notice_type+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Institution"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.placed_by_institution.institution_name+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Created"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$createdDateFmt+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Updated"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$updatedDateFmt+"</span></div></td></tr></tbody></table></div></div>")
                       }
@@ -29,10 +33,8 @@ $(function () {
             }
             if (data.institution_notice) {
                 $.each(data.institution_notice, function(key, value){  // The contents inside institution_notice
-                    var $updatedDate = new Date(value.updated)
-                    var $updatedDateFmt = $createdDate.toLocaleString
-                    var $createdDate = new Date(value.created)
-                    var $createdDateFmt = $createdDate.toLocaleString
+                    var $createdDateFmt = returnDateFmt(value.created);
+                    var $updatedDateFmt = returnDateFmt(value.updated);
                     $lcResponse.append("<div class='row'><div class='col-md-3 col-12 my-auto'><a target='_blank' href="+value.attribution_incomplete_img_url+"><img class='w-100 border-radius-lg shadow-lg' src="+value.attribution_incomplete_img_url+" alt="+value.notice_type+"></a></div><div class='col-md-9 col-12 my-auto'><p class='pe-md-5'>"+value.attribution_incomplete_default_text+"</p><table class='table align-items-center mb-0' style='text-transform:capitalize;'><tbody><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Notice Type"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.notice_type+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Institution"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.institution.institution_name+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Created"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$createdDateFmt+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Updated"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$updatedDateFmt+"</span></div></td></tr></tbody></table></div></div>")
                 });
             }
@@ -45,10 +47,8 @@ $(function () {
             */
             if (data.bclabels) {
                 $.each(data.bclabels, function(key, value){  // The contents inside institution_notice
-                    var $updatedDate = new Date(value.updated)
-                    var $updatedDateFmt = $createdDate.toLocaleString
-                    var $createdDate = new Date(value.created)
-                    var $createdDateFmt = $createdDate.toLocaleString
+                    var $createdDateFmt = returnDateFmt(value.created);
+                    var $updatedDateFmt = returnDateFmt(value.updated);
                     $lcResponse.append("<div class='row'><div class='col-md-3 col-12 my-auto'><a target='_blank' href="+value.img_url+"><img class='w-100 border-radius-lg shadow-lg' src="+value.img_url+" alt="+value.label_type+"></a></div><div class='col-md-9 col-12 my-auto'><p class='pe-md-5'>"+value.default_text+"</p><table class='table align-items-center mb-0' style='text-transform:capitalize;'><tbody><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Label Type"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.label_type+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Label Name"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.name+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Community"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+value.community+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Created"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$createdDateFmt+"</span></div></td></tr><tr><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+"Updated"+"</span></div></td><td class='align-middle text-center'><div class='d-flex align-items-center'><span class='me-2 text-xs'>"+$updatedDateFmt+"</span></div></td></tr></tbody></table></div></div>")
                 });
             }
