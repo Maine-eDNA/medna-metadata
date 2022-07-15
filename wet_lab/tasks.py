37b0ee3ed1ceb3fe2cb71f1134b70a407db2299b
Mel <mkimble88@gmail.com>
2022-07-14 23:08:07 -0400

    added untested routine for ingesting wetlabdocumentation in tasks.py for wet_lab module

--- wet_lab/tasks.py
+++ wet_lab/tasks.py
@@ -203,7 +203,7 @@ def update_record_libraryprep(lib_prep_experiment_name, lib_prep_datetime, proce
             index_removal_method = IndexRemovalMethod.objects.get(index_removal_method_name=index_removal_method)
             quantification_method = QuantificationMethod.objects.get(quant_method_name=quantification_method)
             lib_prep_sop = StandardOperatingProcedure.objects.get(sop_url=lib_prep_sop)
-            extraction, created = LibraryPrep.objects.update_or_create(
+            lib_prep, created = LibraryPrep.objects.update_or_create(
                 lib_prep_experiment_name=lib_prep_experiment_name,
                 extraction=extraction,
                 primer_set=primer_set,
@@ -229,10 +229,81 @@ def update_record_libraryprep(lib_prep_experiment_name, lib_prep_datetime, proce
                 }
             )
         else:
-            extraction, created = None
-        return extraction, created
+            lib_prep, created = None
+        return lib_prep, created
     except Exception as err:
-        raise RuntimeError('** Error: update_record_extraction Failed (' + str(err) + ')')
+        raise RuntimeError('** Error: update_record_libraryprep Failed (' + str(err) + ')')
+
+
+def update_record_pooledlibrary(lib_prep_list, pooled_lib_label,
+                                pooled_lib_datetime,
+                                pooled_lib_barcode, process_location,
+                                quantification_method, pooled_lib_concentration,
+                                pooled_lib_concentration_units, pooled_lib_volume,
+                                pooled_lib_volume_units,
+                                pooled_lib_notes):
+    try:
+        # convert to lowercase to prevent mismatches due to camelcase
+        if lib_prep_list and pooled_lib_barcode:
+            pooled_lib_barcode = SampleBarcode.objects.get(barcode_slug=pooled_lib_barcode.lower())
+            process_location = ProcessLocation.objects.get(process_location_name=process_location)
+            quantification_method = QuantificationMethod.objects.get(quant_method_name=quantification_method)
+            pooled_lib, created = PooledLibrary.objects.update_or_create(
+                pooled_lib_barcode=pooled_lib_barcode,
+                defaults={
+                    'pooled_lib_label': pooled_lib_label,
+                    'pooled_lib_datetime': pooled_lib_datetime,
+                    'pooled_lib_barcode': pooled_lib_barcode,
+                    'process_location': process_location,
+                    'quantification_method': quantification_method,
+                    'pooled_lib_concentration': pooled_lib_concentration,
+                    'pooled_lib_concentration_units': pooled_lib_concentration_units,
+                    'pooled_lib_volume': pooled_lib_volume,
+                    'pooled_lib_volume_units': pooled_lib_volume_units,
+                    'pooled_lib_notes': pooled_lib_notes,
+                }
+            )
+            # ManyToManyFields must be added separately though set(). clear=True clears the fields first
+            pooled_lib.library_prep.set(lib_prep_list, clear=True)
+        else:
+            pooled_lib, created = None
+        return pooled_lib, created
+    except Exception as err:
+        raise RuntimeError('** Error: update_record_pooledlibrary Failed (' + str(err) + ')')
+
+
+def update_record_runprep(pooled_lib_list, run_prep_label,
+                          run_prep_datetime,
+                          process_location,
+                          quantification_method,
+                          run_prep_concentration, run_prep_concentration_units,
+                          run_prep_phix_spike_in, run_prep_phix_spike_in_units,
+                          run_prep_notes):
+    try:
+        # convert to lowercase to prevent mismatches due to camelcase
+        if pooled_lib_list and run_prep_label:
+            process_location = ProcessLocation.objects.get(process_location_name=process_location)
+            quantification_method = QuantificationMethod.objects.get(quant_method_name=quantification_method)
+            run_prep, created = RunPrep.objects.update_or_create(
+                run_prep_label=run_prep_label,
+                defaults={
+                    'run_prep_datetime': run_prep_datetime,
+                    'process_location': process_location,
+                    'quantification_method': quantification_method,
+                    'run_prep_concentration': run_prep_concentration,
+                    'run_prep_concentration_units': run_prep_concentration_units,
+                    'run_prep_phix_spike_in': run_prep_phix_spike_in,
+                    'run_prep_phix_spike_in_units': run_prep_phix_spike_in_units,
+                    'run_prep_notes': run_prep_notes,
+                }
+            )
+            # ManyToManyFields must be added separately though set(). clear=True clears the fields first
+            run_prep.pooled_library.set(pooled_lib_list, clear=True)
+        else:
+            run_prep, created = None
+        return run_prep, created
+    except Exception as err:
+        raise RuntimeError('** Error: update_record_runprep Failed (' + str(err) + ')')
 
 
 def update_queryset_fastq_file(queryset):
@@ -251,9 +322,10 @@ def update_queryset_fastq_file(queryset):
 def parse_wetlabdoc_file(wetlabdoc_file):
     try:
         update_count = 0
+        lib_prep_list = dict()
+        pooled_lib_list = dict()
         wetlabdoc_datafile = wetlabdoc_file.wetlabdoc_datafile
         pk = wetlabdoc_file.pk
-        # record = dict({})
         # file = wetlabdoc_datafile.read().decode('utf-8')
         # csv_data = csv.reader(StringIO(file), delimiter=',')
         extr_libprep_df = pd.read_excel(wetlabdoc_datafile, sheet_name=1)
@@ -294,60 +366,119 @@ def parse_wetlabdoc_file(wetlabdoc_file):
             if extr_created:
                 update_count += 1
 
-            library_prep_location = extr_libprep_df['library_prep_location'][row]
-            library_prep_datetime = extr_libprep_df['library_prep_datetime'][row]
-            library_prep_amplification_method = extr_libprep_df['library_prep_amplification_method'][row]
-            library_prep_primer_set_name = extr_libprep_df['library_prep_primer_set_name'][row]
-            library_prep_index_removal_method = extr_libprep_df['library_prep_index_removal_method'][row]
-            library_prep_size_selection_method = extr_libprep_df['library_prep_size_selection_method'][row]
-            library_prep_experiment_name = extr_libprep_df['library_prep_experiment_name'][row]
-            library_prep_quantification_method = extr_libprep_df['library_prep_quantification_method'][row]
-            qubit_results = extr_libprep_df['qubit_results'][row]
-            qubit_units = extr_libprep_df['qubit_units'][row]
-            qpcr_results = extr_libprep_df['qpcr_results'][row]
-            qpcr_units = extr_libprep_df['qpcr_units'][row]
-            library_prep_final_concentration = extr_libprep_df['library_prep_final_concentration'][row]
-            library_prep_final_concentration_units = extr_libprep_df['library_prep_final_concentration_units'][row]
-            library_prep_kit = extr_libprep_df['library_prep_kit'][row]
-            library_prep_type = extr_libprep_df['library_prep_type'][row]
-            library_prep_thermal_sop_url = extr_libprep_df['library_prep_thermal_sop_url'][row]
-            library_prep_notes = extr_libprep_df['library_prep_notes'][row]
+                library_prep_location = extr_libprep_df['library_prep_location'][row]
+                library_prep_datetime = extr_libprep_df['library_prep_datetime'][row]
+                library_prep_amplification_method = extr_libprep_df['library_prep_amplification_method'][row]
+                library_prep_primer_set_name = extr_libprep_df['library_prep_primer_set_name'][row]
+                library_prep_index_removal_method = extr_libprep_df['library_prep_index_removal_method'][row]
+                library_prep_size_selection_method = extr_libprep_df['library_prep_size_selection_method'][row]
+                library_prep_experiment_name = extr_libprep_df['library_prep_experiment_name'][row]
+                library_prep_quantification_method = extr_libprep_df['library_prep_quantification_method'][row]
+                qubit_results = extr_libprep_df['qubit_results'][row]
+                qubit_units = extr_libprep_df['qubit_units'][row]
+                qpcr_results = extr_libprep_df['qpcr_results'][row]
+                qpcr_units = extr_libprep_df['qpcr_units'][row]
+                library_prep_final_concentration = extr_libprep_df['library_prep_final_concentration'][row]
+                library_prep_final_concentration_units = extr_libprep_df['library_prep_final_concentration_units'][row]
+                library_prep_kit = extr_libprep_df['library_prep_kit'][row]
+                library_prep_type = extr_libprep_df['library_prep_type'][row]
+                lib_prep_layout = extr_libprep_df['library_prep_layout'][row]
+                lib_prep_thermal_cond = extr_libprep_df['library_prep_thermal_conditions'][row]
+                lib_prep_sop = extr_libprep_df['library_prep_sop_url'][row]
+                lib_prep_notes = extr_libprep_df['library_prep_notes'][row]
 
-            # update_record_libraryprep(library_prep_experiment_name, library_prep_datetime, library_prep_location,
-            #                           extraction, library_prep_amplification_method, library_prep_primer_set_name,
-            #                           library_prep_size_selection_method, library_prep_index_removal_method,
-            #                           library_prep_quantification_method, qubit_results, qubit_units,
-            #                           qpcr_results, qpcr_units,
-            #                           library_prep_final_concentration, library_prep_final_concentration_units,
-            #                           library_prep_kit, library_prep_type, lib_prep_layout, lib_prep_thermal_cond,
-            #                           lib_prep_sop, lib_prep_notes)
+                lib_prep, lp_created = update_record_libraryprep(library_prep_experiment_name, library_prep_datetime,
+                                                                 library_prep_location,
+                                                                 extraction, library_prep_amplification_method,
+                                                                 library_prep_primer_set_name,
+                                                                 library_prep_size_selection_method,
+                                                                 library_prep_index_removal_method,
+                                                                 library_prep_quantification_method,
+                                                                 qubit_results, qubit_units,
+                                                                 qpcr_results, qpcr_units,
+                                                                 library_prep_final_concentration, library_prep_final_concentration_units,
+                                                                 library_prep_kit, library_prep_type,
+                                                                 lib_prep_layout, lib_prep_thermal_cond,
+                                                                 lib_prep_sop, lib_prep_notes)
+                if lp_created:
+                    update_count += 1
+                    if library_prep_experiment_name in lib_prep_list:
+                        # https://www.journaldev.com/40231/check-if-a-key-exists-in-python-dictionary#:~:text=The%20get()%20method%20in,by%20the%20user%20is%20returned.
+                        # https://stackoverflow.com/questions/2285874/python-dictionary-that-maps-strings-to-a-set-of-strings
+                        lib_prep_list[library_prep_experiment_name].add(lib_prep)
+                    else:
+                        lib_prep_list[library_prep_experiment_name] = set()
+                        lib_prep_list[library_prep_experiment_name].add(lib_prep)
 
         pooledlib_df = pd.read_excel(wetlabdoc_datafile, sheet_name=2)
         for row in pooledlib_df:
             # POOLED LIBRARY
-            pooled_library_label = extr_libprep_df['pooled_library_label'][row]
-            pooled_library_location = extr_libprep_df['pooled_library_location'][row]
-            pooled_library_datetime = extr_libprep_df['pooled_library_datetime'][row]
-            pooled_library_quantification_method = extr_libprep_df['pooled_library_quantification_method'][row]
-            pooled_library_concentration = extr_libprep_df['pooled_library_concentration'][row]
-            pooled_library_concentration_units = extr_libprep_df['pooled_library_concentration_units'][row]
-            pooled_library_notes = extr_libprep_df['pooled_library_notes'][row]
+            library_prep_experiment_name_list = pooledlib_df['library_prep_experiment_name_list'][row]
+            pooled_library_barcode = pooledlib_df['pooled_library_barcode'][row]
+            pooled_library_barcode = pooled_library_barcode.lower()
+            pooled_library_label = pooledlib_df['pooled_library_label'][row]
+            pooled_library_location = pooledlib_df['pooled_library_location'][row]
+            pooled_library_datetime = pooledlib_df['pooled_library_datetime'][row]
+            pooled_library_quantification_method = pooledlib_df['pooled_library_quantification_method'][row]
+            pooled_library_concentration = pooledlib_df['pooled_library_concentration'][row]
+            pooled_library_concentration_units = pooledlib_df['pooled_library_concentration_units'][row]
+            pooled_library_volume = pooledlib_df['pooled_library_volume'][row]
+            pooled_library_volume_units = pooledlib_df['pooled_library_volume_units'][row]
+            pooled_library_notes = pooledlib_df['pooled_library_notes'][row]
+
+            related_lib_prep_list = [x for x in lib_prep_list if x in library_prep_experiment_name_list]
+
+            pooled_lib, pl_created = update_record_pooledlibrary(related_lib_prep_list, pooled_library_label,
+                                                                 pooled_library_datetime,
+                                                                 pooled_library_barcode, pooled_library_location,
+                                                                 pooled_library_quantification_method, pooled_library_concentration,
+                                                                 pooled_library_concentration_units, pooled_library_volume,
+                                                                 pooled_library_volume_units,
+                                                                 pooled_library_notes)
+
+            if pooled_lib:
+                update_count += 1
+                if pooled_library_barcode in pooled_lib_list:
+                    # https://www.journaldev.com/40231/check-if-a-key-exists-in-python-dictionary#:~:text=The%20get()%20method%20in,by%20the%20user%20is%20returned.
+                    # https://stackoverflow.com/questions/2285874/python-dictionary-that-maps-strings-to-a-set-of-strings
+                    pooled_lib_list[pooled_library_barcode].add(pooled_lib)
+                else:
+                    pooled_lib_list[pooled_library_barcode] = set()
+                    pooled_lib_list[pooled_library_barcode].add(pooled_lib)
+
         runprep_df = pd.read_excel(wetlabdoc_datafile, sheet_name=3)
         for row in runprep_df:
             # RUN PREP
-            run_prep_location = extr_libprep_df['run_prep_location'][row]
-            run_prep_datetime = extr_libprep_df['run_prep_datetime'][row]
-            sequencing_location = extr_libprep_df['sequencing_location'][row]
-            phix_spike_in = extr_libprep_df['phix_spike_in'][row]
-            phix_spike_in_units = extr_libprep_df['phix_spike_in_units'][row]
-            final_library_quantification_method = extr_libprep_df['final_library_quantification_method'][row]
-            final_library_concentration = extr_libprep_df['final_library_concentration'][row]
-            final_library_concentration_units = extr_libprep_df['final_library_concentration_units'][row]
-            run_prep_notes = extr_libprep_df['run_prep_notes'][row]
+            pooled_library_barcode_list =  runprep_df['pooled_library_barcode_list'][row]
+            run_prep_label = runprep_df['run_prep_label'][row]
+            run_prep_datetime = runprep_df['run_prep_datetime'][row]
+            sequencing_location = runprep_df['sequencing_location'][row]
+            phix_spike_in = runprep_df['phix_spike_in'][row]
+            phix_spike_in_units = runprep_df['phix_spike_in_units'][row]
+            final_library_quantification_method = runprep_df['final_library_quantification_method'][row]
+            final_library_concentration = runprep_df['final_library_concentration'][row]
+            final_library_concentration_units = runprep_df['final_library_concentration_units'][row]
+            run_prep_notes = runprep_df['run_prep_notes'][row]
 
-        update_record_wetlabdoc_file(pk, library_prep_location, library_prep_datetime, pooled_library_label,
-                                     pooled_library_location, pooled_library_datetime, run_prep_location,
-                                     run_prep_datetime, sequencing_location)
+            related_pooled_lib_list = [x for x in pooled_lib_list if x in pooled_library_barcode_list]
+
+            run_prep, rp_created = update_record_runprep(related_pooled_lib_list, run_prep_label,
+                                                         run_prep_datetime,
+                                                         sequencing_location,
+                                                         final_library_quantification_method,
+                                                         final_library_concentration, final_library_concentration_units,
+                                                         phix_spike_in, phix_spike_in_units,
+                                                         run_prep_notes)
+            if rp_created:
+                update_count += 1
+
+        wet_lab_doc, weblabdoc_created = update_record_wetlabdoc_file(pk, library_prep_location,
+                                                                      library_prep_datetime, pooled_library_label,
+                                                                      pooled_library_location, pooled_library_datetime,
+                                                                      sequencing_location, run_prep_datetime,
+                                                                      sequencing_location)
+        if weblabdoc_created:
+            update_count += 1
         return update_count
     except Exception as err:
         raise RuntimeError('** Error: update_record_wetlabdoc_file Failed (' + str(err) + ')')
