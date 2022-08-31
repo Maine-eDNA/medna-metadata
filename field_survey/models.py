17f079588c0a1b1676a219e9f8dfcc2e65c0f496
Mel <mkimble88@gmail.com>
2022-08-31 08:59:50 -0400

    removed import in save method out of if statement to ensure modules are loaded in field_survey module

--- field_survey/models.py
+++ field_survey/models.py
@@ -280,8 +280,8 @@ class FieldSample(DateTimeUserMixin):
         return '{barcode}: {gid}'.format(barcode=self.barcode_slug, gid=self.sample_global_id)
 
     def save(self, *args, **kwargs):
+        from sample_label.models import update_barcode_sample_type, get_field_sample_sample_type
         if self.field_sample_barcode:
-            from sample_label.models import update_barcode_sample_type, get_field_sample_sample_type
             # update_barcode_sample_type must come before creating barcode_slug
             # because need to grab old barcode_slug value on updates
             # update barcode to type == Field Sample
