b046a8257ae7a361805e8cde9656f6f751f5e764
mkimble88 <mkimble88@gmail.com>
2023-11-25 20:52:13 -0400

    Updated logging.info for CharSerializerExportMixin and SerializerExportMixin to display source and print mapped item as list

--- utility/serializers.py
+++ utility/serializers.py
@@ -355,9 +355,9 @@ class SerializerExportMixin(ExportMixin):
     def get_table_data(self):
         selected_column_ids = self.request.GET.get('_selected_column_ids', None)
         if selected_column_ids:
-            logging.info('selected_column_ids pre-map: ["{}"].'.format(selected_column_ids))
+            logging.info('SerializerExportMixin: selected_column_ids pre-map: ["{}"].'.format(selected_column_ids))
             selected_column_ids = map(int, selected_column_ids.split(','))
-            logging.info('selected_column_ids post-map: ["{}"].'.format(selected_column_ids))
+            logging.info('SerializerExportMixin: selected_column_ids post-map: ["{}"].'.format(list(selected_column_ids)))
             return super().get_table_data().filter(pk__in=selected_column_ids)
         return super().get_table_data()
 
@@ -385,9 +385,9 @@ class CharSerializerExportMixin(ExportMixin):
     def get_table_data(self):
         selected_column_ids = self.request.GET.get('_selected_column_ids', None)
         if selected_column_ids:
-            logging.info('selected_column_ids pre-map: ["{}"].'.format(selected_column_ids))
+            logging.info('CharSerializerExportMixin: selected_column_ids pre-map: ["{}"].'.format(selected_column_ids))
             # selected_column_ids = map(int, selected_column_ids.split(','))
             selected_column_ids = map(str, selected_column_ids.split(','))
-            logging.info('selected_column_ids post-map: ["{}"].'.format(selected_column_ids))
+            logging.info('CharSerializerExportMixin: selected_column_ids post-map: ["{}"].'.format(list(selected_column_ids)))
             return super().get_table_data().filter(pk__in=selected_column_ids)
         return super().get_table_data()
